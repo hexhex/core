97c90580521238ffc92e6a18f58849e3c2735dd8
Roman Schindlauer <roman@kr.tuwien.ac.at>
2006-12-06 15:10:02 +0000

    * benchmarks/reviewers-pureasp.dlv: Added benchmark.
    * benchmarks/run.sh: Modified "time"-call.

--- benchmarks/run.sh
+++ benchmarks/run.sh
@@ -10,7 +10,7 @@ TOT=0
 
 for (( i = 1 ;  i <= $LOOP;  i++ ))
 do
-    T=`(time $* >/dev/null) 2>&1 | grep real | cut -dm -f2 | cut -ds -f1`
+    T=`(time -p $* >/dev/null) 2>&1 | grep real | cut -d ' ' -f2`
     echo "$i: $*: $T"
     TOT=$(echo "scale=$SCALE; $TOT+$T" | bc)
 done
