b286a397a7914a8667de0c003bf5f932e0afdc9a
Thomas Krennwallner <tkren@kr.tuwien.ac.at>
2008-01-20 19:50:03 +0000

    Copy libltdl.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
-autoreconf -i -W all
 
+LT=`which libtoolize`
+
+if [ -x "$LT" ]; then
+    LT=libtoolize
+else
+    LT=glibtoolize
+fi
+
+# copy libltdl into working repository
+$LT -c -f --ltdl
+
+# rebuild autotool files
+autoreconf -f -i -W all
