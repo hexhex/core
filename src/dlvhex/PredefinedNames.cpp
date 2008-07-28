3c062ed272d26bdf96698c67da3795c59ec3337f
Thomas Krennwallner <tkren@kr.tuwien.ac.at>
2008-07-28 06:38:54 +0000

    Add.

---
+++ src/dlvhex/PredefinedNames.cpp
@@ -0,0 +1,46 @@
+/* dlvhex -- Answer-Set Programming with external interfaces.
+ * Copyright (C) 2005, 2006, 2007 Roman Schindlauer
+ * 
+ * This file is part of dlvhex.
+ *
+ * dlvhex is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * dlvhex is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with dlvhex; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ */
+
+
+/**
+ * @file PredefinedNames.cpp
+ * @author Thomas Krennwallner
+ * @date Wed Jul 23 10:06:13 CEST 2008
+ *
+ * @brief Predefined names.
+ *
+ *
+ */
+
+#include "dlvhex/PredefinedNames.h"
+
+
+DLVHEX_NAMESPACE_BEGIN
+
+const std::string PredefinedNames::WEAKHEAD("h__wch_");
+const std::string PredefinedNames::FLPHEAD("h__flp_");
+const std::string PredefinedNames::HOATOM("a__");
+
+DLVHEX_NAMESPACE_END
+
+// Local Variables:
+// mode: C++
+// End:
