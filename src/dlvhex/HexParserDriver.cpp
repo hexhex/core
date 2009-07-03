78e06ed35f9a70a23b6f4f66ca5570386fd44775
Peter Schüller <ps@kr.tuwien.ac.at>
2009-07-03 12:15:55 +0000

    starting to implement dlvhex AST generation

--- src/dlvhex/HexParserDriver.cpp
+++ src/dlvhex/HexParserDriver.cpp
@@ -39,14 +39,12 @@
 #include <boost/spirit/utility/confix.hpp>
 #include <boost/spirit/iterator/multi_pass.hpp>
 #include <boost/spirit/iterator/position_iterator.hpp>
-// parse_tree is useful for debugging (grammar and ast creation)
 #include <boost/spirit/tree/parse_tree.hpp>
-// ast will be useful for creating the AST
-#include <boost/spirit/tree/ast.hpp>
 
 #include <iostream>
 #include <sstream>
 #include <fstream>
+#include <cassert>
 
 DLVHEX_NAMESPACE_BEGIN
 
@@ -54,6 +52,7 @@ DLVHEX_NAMESPACE_BEGIN
 
 namespace sp = boost::spirit;
 
+using sp::rule;
 using sp::ch_p;
 using sp::str_p;
 using sp::space_p;
@@ -63,29 +62,39 @@ using sp::space_p;
 namespace
 {
 
+//
+// parsing and creating the boost::spirit parse tree (PT)
+//
+
+// iterator which remembers file positions (useful for error messages)
 typedef sp::position_iterator<const char*> pos_iterator_t;
+// node type for spirit PT
+typedef sp::tree_match<pos_iterator_t>::node_t node_t;
 
-template<typename NodeT>
-void printSpiritAST(std::ostream& o, NodeT& node, const std::string& indent="");
+// boost::spirit PT debugging
+void printSpiritPT(std::ostream& o, const node_t& node, const std::string& indent="");
 
-// see "The Grammar" in the spirit docs
+// the grammar of hex (see "The Grammar" in the spirit docs)
 struct HexSpiritGrammar:
   public sp::grammar<HexSpiritGrammar>
 {
-  template<typename ScannerT>
+  enum RuleTags { Root = 1, Maxint, Namespace, Rule, Constraint, WeakConstraint, Body, Number };
+
+  // S = ScannerT
+  template<typename S>
   struct definition
   {
-  public:
-    typedef typename sp::rule<ScannerT> rule_t;
+    // shortcut
+    typedef typename sp::rule<S> rule_t;
+    typedef sp::parser_context<> c;
+    template<int Tag> struct tag: public sp::parser_tag<Tag> {};
 
-  public:
     definition(HexSpiritGrammar const& self);
-    rule_t const& start() const { return root; }
+    rule< S, c, tag<Root> > const& start() const { return root; }
 
-  protected:
     rule_t ident;
     rule_t var;
-    rule_t number;
+    rule< S, c, tag<Number> > number;
     rule_t ident_or_var;
     rule_t ident_or_var_or_number;
     rule_t cons;
@@ -104,67 +113,73 @@ struct HexSpiritGrammar:
     rule_t disj;
 		rule_t neg;
     rule_t user_pred;
-    rule_t body; // body of a rule or constraint or aggregate
+    rule< S, c, tag<Body> > body; // body of a rule or constraint or aggregate
+    rule< S, c, tag<Maxint> > maxint;
+    rule< S, c, tag<Namespace> > namespace_;
+    rule< S, c, tag<Rule> > rule_;
+    rule< S, c, tag<Constraint> > constraint;
+    rule< S, c, tag<WeakConstraint> > wconstraint;
 		rule_t clause;
-    rule_t root;
+    rule< S, c, tag<Root> > root;
   };
 };
 
-struct lift_node_op
-{
-	template <typename MatchT>
-	void operator()(MatchT& m) const
-	{    
-		// if match has one tree with more than one child, replace match by these children
-		// (do as if the rule were inlined in the other rule)
-		if (m.trees.size() == 1 && m.trees.begin()->children.size() > 1)
-		{
-			std::swap(m.trees, m.trees.begin()->children);
-		}    
-	}    
-};
-const sp::node_parser_gen<lift_node_op> lift_node_d =
-    sp::node_parser_gen<lift_node_op>();
+// newer boost requires this
+//#define flatten sp::reduced_node_d
+// KBS boost requires this (this does not work correctly in newer boost)
+#define flatten sp::leaf_node_d
 
 // impl of HexSpiritGrammar
 template<typename ScannerT>
 HexSpiritGrammar::definition<ScannerT>::definition(HexSpiritGrammar const&)
 {
-	// shortcut for sp::discard_node_d()
-	const sp::node_parser_gen<sp::discard_node_op> rm =
-		sp::node_parser_gen<sp::discard_node_op>();
+  // shortcut for sp::discard_node_d()
+  const sp::node_parser_gen<sp::discard_node_op> rm =
+    sp::node_parser_gen<sp::discard_node_op>();
 
   // identifier or string
   ident
-		= sp::reduced_node_d[sp::regex_p("[a-z][a-zA-Z0-9_]*")]
-		| sp::reduced_node_d[sp::confix_p("\"", *sp::anychar_p, "\"")];
+    = flatten[sp::regex_p("[a-z][a-zA-Z0-9_]*")]
+    | flatten[sp::confix_p("\"", *sp::anychar_p, "\"")];
   // variable
   var
-		= sp::reduced_node_d[sp::regex_p("[A-Z][a-zA-Z0-9_]*")];
+    = flatten[sp::regex_p("[A-Z][a-zA-Z0-9_]*")];
   // nonnegative integer
-  number = sp::reduced_node_d[+sp::digit_p];
-  ident_or_var = ident | var;
-  ident_or_var_or_number = ident | var | number;
-  aggregate_leq_binop = str_p("<=") | '<';
-  aggregate_geq_binop = str_p(">=") | '>';
-  aggregate_binop = aggregate_leq_binop | aggregate_geq_binop | str_p("==") | '=';
-  binop = aggregate_binop | str_p("!=") | str_p("<>");
-  tertop = ch_p('*') | '+';
-  cons = str_p(":-") | str_p("<-");
+  number
+    = flatten[+sp::digit_p];
+  ident_or_var
+    = ident | var;
+  ident_or_var_or_number
+    = ident | var | number;
+  aggregate_leq_binop
+    = str_p("<=") | '<';
+  aggregate_geq_binop
+    = str_p(">=") | '>';
+  aggregate_binop
+    = aggregate_leq_binop | aggregate_geq_binop | str_p("==") | '=';
+  binop
+    = aggregate_binop | str_p("!=") | str_p("<>");
+  tertop
+    = ch_p('*') | '+';
+  cons
+    = str_p(":-") | str_p("<-");
   // identifiers, variables, numbers, anonymous variables
-  term = ident_or_var_or_number | ch_p('_');
-  terms = term >> *(rm[ch_p(',')] >> term);
-	// strong negation
-	neg = ch_p('-')|ch_p('~');
+  term
+    = ident_or_var_or_number | ch_p('_');
+  terms
+    = term >> *(rm[ch_p(',')] >> term);
+  neg
+    = ch_p('-')|ch_p('~');
   user_pred
-    = ( ( !neg >> ident_or_var >> '(' >> lift_node_d[terms] >> ')')
-			| ( '(' >> lift_node_d[terms] >> ')' )
+    = ( ( !neg >> ident_or_var >> '(' >> terms >> ')')
+      | ( '(' >> terms >> ')' )
       | ( !neg >> ident_or_var)
       );
-  external_atom = ch_p('&') >> ident >> !('[' >> lift_node_d[!terms] >> ']') >> !('(' >> lift_node_d[!terms] >> ')');
+  external_atom
+    = ch_p('&') >> ident >> !('[' >> !terms >> ']') >> !('(' >> !terms >> ')');
   aggregate_pred
     = (str_p("#any")|"#avg"|"#count"|"#max"|"#min"|"#sum"|"#times")
-    >> '{' >> lift_node_d[terms] >> ':' >> lift_node_d[body] >> '}';
+    >> '{' >> terms >> ':' >> body >> '}';
   aggregate
     = (term >> aggregate_binop >> aggregate_pred)
     | (aggregate_pred >> aggregate_binop >> term)
@@ -182,48 +197,96 @@ HexSpiritGrammar::definition<ScannerT>::definition(HexSpiritGrammar const&)
     = builtin_pred
     | ( !(str_p("not")|str_p("non")) >> (user_pred | external_atom | aggregate) );
   disj = user_pred >> *(rm[ch_p('v')] >> user_pred);
-  body
-    = literal >> *(rm[ch_p(',')] >> literal);
-	clause
-		=   (str_p("#maxint") >> '=' >> number >> rm[ch_p('.')]) // TODO: previously implemented in a wrong way (no "." at the end!)
-      // TODO: sp::eol_p should be added, but this does not work (because of skip parser?)
-      | (str_p("#namespace") >> '(' >> ident >> rm[ch_p(',')] >> ident >> ')') // TODO: change and add "." at the end?
-      | // rule (optional body/condition)
-        (disj >> !(cons >> !body) >> rm[ch_p('.')])
-      | // constraint
-        (cons >> sp::gen_pt_node_d[body] >> rm[ch_p('.')])
-      | // weak constraint
-        (str_p(":~") >> body >> rm[ch_p('.')] >>
-				// optional weight
-				 !( rm[ch_p('[')]
-					  >> !ident_or_var_or_number
-						>> rm[ch_p(':')]
-						>> !ident_or_var_or_number
-						>> rm[ch_p(']')]
-				  ));
-  root =
-		*( // comment
-       rm[sp::comment_p("%")]
-		 | clause
-		 | sp::eol_p
-		 )
-     // end_p enforces a "full" match in case of success even with trailing newlines
-     >> sp::end_p;
+  body = literal >> *(rm[ch_p(',')] >> literal);
+  // TODO: previously implemented in a wrong way (no "." at the end!)
+  maxint = str_p("#maxint") >> '=' >> number >> rm[ch_p('.')];
+  // TODO: change #namespace to have "." at the end?
+  // TODO: sp::eol_p should be added, but this does not work (because of skip parser?)
+  namespace_ = str_p("#namespace") >> '(' >> ident >> rm[ch_p(',')] >> ident >> ')';
+  // rule (optional body/condition)
+  rule_ = disj >> !(cons >> !body) >> rm[ch_p('.')];
+  // constraint
+  constraint =  (cons >> body >> rm[ch_p('.')]);
+  // weak constraint
+  wconstraint =
+    str_p(":~") >> body >> rm[ch_p('.')]
+    // optional weight
+    >> !( rm[ch_p('[')]
+       >> !ident_or_var_or_number
+       >> rm[ch_p(':')]
+       >> !ident_or_var_or_number
+       >> rm[ch_p(']')]
+        );
+  clause
+    = maxint | namespace_ | rule_ | constraint | wconstraint;
+  root
+    = *( // comment
+         rm[sp::comment_p("%")]
+       | clause
+       )
+       // end_p enforces a "full" match (in case of success) even with trailing newlines
+       >> !sp::end_p;
 }
 
-// AST debugging
-
-template<typename NodeT>
-void printSpiritAST(std::ostream& o, NodeT& node, const std::string& indent)
+void printSpiritPT(std::ostream& o, const node_t& node, const std::string& indent)
 {
-  o << indent << "'" << std::string(node.value.begin(), node.value.end()) << "'" << std::endl;
+  o << indent << "'" << std::string(node.value.begin(), node.value.end()) << "'\t\t\t(" << node.value.id().to_long() << ")" << std::endl;
   if( !node.children.empty() )
   {
     std::string cindent(indent + "  ");
-    for(typename NodeT::const_tree_iterator it = node.children.begin(); it != node.children.end(); ++it)
+    for(node_t::const_tree_iterator it = node.children.begin(); it != node.children.end(); ++it)
     {
-      printSpiritAST(o, *it, cindent);
+      printSpiritPT(o, *it, cindent);
     }
+  }
+}
+
+//
+// creating the dlvhex AST from the boost::spirit PT
+//
+
+void createASTFromClause(node_t& node, Program& program, AtomSet& edb);
+void createAST(node_t& node, Program& program, AtomSet& edb)
+{
+  // node is from "root" rule
+  assert(node.value.id() == HexSpiritGrammar::Root);
+  for(node_t::tree_iterator it = node.children.begin(); it != node.children.end(); ++it)
+    createASTFromClause(*it, program, edb);
+}
+
+std::string createStringFromNumber(node_t& node)
+{
+  assert(node.value.id() == HexSpiritGrammar::Number);
+  printSpiritPT(std::cerr, node);
+  return std::string(node.children[0].value.begin(), node.children[0].value.end());
+}
+
+void createASTFromClause(node_t& node, Program& program, AtomSet& edb)
+{
+  // node is from "clause" rule
+  assert(node.children.size() == 1);
+  node_t& child = node.children[0];
+  switch(child.value.id().to_long())
+  {
+  case HexSpiritGrammar::Maxint:
+    printSpiritPT(std::cerr, child);
+    assert(child.children.size() == 3);
+    Globals::Instance()->maxint = "#maxint=" + createStringFromNumber(child.children[2]) + ".";
+    break;
+  case HexSpiritGrammar::Namespace:
+    printSpiritPT(std::cerr, child);
+    break;
+  case HexSpiritGrammar::Rule:
+    printSpiritPT(std::cerr, child);
+    break;
+  case HexSpiritGrammar::Constraint:
+    printSpiritPT(std::cerr, child);
+    break;
+  case HexSpiritGrammar::WeakConstraint:
+    printSpiritPT(std::cerr, child);
+    break;
+  default:
+    assert(false && "encountered unknown node in createASTFromClause!");
   }
 }
 
@@ -274,36 +337,38 @@ HexParserDriver::parse(std::istream& is,
   buf << is.rdbuf();
   std::string input = buf.str();
 
-  // parse with spirit
-  try
+  // create grammar
+  HexSpiritGrammar grammar;
+
+  // remember line numbers
+  pos_iterator_t it_begin(input.c_str(), input.c_str()+input.size());
+  pos_iterator_t it_end;
+
+  // parse ast
+  sp::tree_parse_info<pos_iterator_t> info =
+    sp::pt_parse(it_begin, it_end, grammar, sp::space_p);
+
+  // debug output
+  std::cerr << "spirit match: full=" << info.full << ", match=" << info.match << std::endl;
+  node_t::children_t::iterator itt;
+  for(itt = info.trees.begin(); itt != info.trees.end(); ++itt)
   {
-    HexSpiritGrammar grammar;
-
-    // while remembering line numbers
-    pos_iterator_t it_begin(input.c_str(), input.c_str()+input.size());
-    pos_iterator_t it_end;
-
-    // TODO: if debugging, parse with pt_parse and output with printSpiritAST, otherwise use ast_parse directly
-
-    // parse generic tree (no special ast actions are used except lexeme_p)
-    sp::tree_parse_info<pos_iterator_t> info =
-      sp::ast_parse(it_begin, it_end, grammar, sp::space_p);
-
-    // debug output
-    std::cerr << "spirit match: full=" << info.full << ", match=" << info.match << std::endl;
-    typedef sp::tree_match<pos_iterator_t>::node_t node_t;
-    node_t::children_t::iterator itt;
-    for(itt = info.trees.begin(); itt != info.trees.end(); ++itt)
-    {
-      std::cerr << "got tree from spirit:" << std::endl;
-      printSpiritAST(std::cerr, *itt);
-    }
+    std::cerr << "got tree from spirit:" << std::endl;
+    printSpiritPT(std::cerr, *itt);
   }
-  catch(const std::exception& e)
-  {
-    std::cerr << "caught spirit exception: " << e.what() << std::endl;
-    // TODO handle exception better
-  }
+
+  // successful parse?
+  if( !info.full )
+    throw SyntaxError("Could not parse complete input!",
+        info.stop.get_position().line, this->source);
+
+  // if this is not ok, there is some bug and the following code will be incomplete
+  assert(info.trees.size() == 1);
+
+  // create dlvhex AST from spirit (rudimentary) AST
+  createAST(*info.trees.begin(), program, EDB);
+
+  return;
 
   // old parser code
   yy::HexParser parser(this, program, EDB);
