e7e12c6d9eec7e1a9effe69b0016a2ec3040d572
Seif El-Din Bairakdar <seif.bairakdar@gmail.com>
2009-12-01 14:37:23 +0000

    Add Seif's files.

---
+++ Atom.cpp
@@ -0,0 +1,28 @@
+
+#include "Atom.h"
+#include <iostream>
+using namespace std;
+Atom::Atom() {
+	this->name = "@";
+}
+Atom::Atom(const Atom& atom){
+	name = atom.name;
+}
+Atom::Atom(string name) {
+	this->name = name;
+}
+Atom::~Atom() {
+	// TODO Auto-generated destructor stub
+}
+void Atom::print(){
+	cout << name << endl;
+}
+bool Atom::operator==( Atom& atom) const{
+	return (name == atom.name);
+}
+bool Atom::operator!=( Atom &atom) const {
+	return !(*this == atom);
+}
+bool Atom::operator<( Atom &atom) const {
+	return name < atom.name;
+}
