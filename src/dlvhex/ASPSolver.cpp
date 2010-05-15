8591069b1392270638a69a3735cd9c68fe2917c6
Peter Schüller <ps@kr.tuwien.ac.at>
2010-05-15 03:46:28 +0000

    added ASPStringSolver, a possibility for calling dlv from C++

--- src/dlvhex/ASPSolver.cpp
+++ src/dlvhex/ASPSolver.cpp
@@ -32,6 +32,7 @@
 
 
 #include "dlvhex/ASPSolver.h"
+#include "dlvhex/DLVresultParserDriver.h"
 
 #include <cassert>
 
@@ -69,12 +70,78 @@ ASPSolverComposite::solve(const Program& p, const AtomSet& s, std::vector<AtomSe
 }
 
 
+ASPStringSolver::ASPStringSolver(Process& proc):
+	proc(proc)
+{
+}
+
+ASPStringSolver::~ASPStringSolver()
+{
+}
+
+//! give a DLV program as a string to this function and it returns the answer sets (or throws)
+void
+ASPStringSolver::solve(const std::string& input, std::vector<AtomSet>& as) throw (FatalError)
+{
+  int retcode = -1;
+  
+  try
+    {
+      DEBUG_START_TIMER;
+
+      proc.spawn();
+      proc.getOutput() << input << std::endl;
+      proc.endoffile();
+
+      // parse result
+      DLVresultParserDriver parser;
+      parser.parse(proc.getInput(), as);
+      
+      // get exit code of process
+      retcode = proc.close();
+
+      //                123456789-123456789-123456789-123456789-123456789-123456789-123456789-123456789-
+      DEBUG_STOP_TIMER("Calling LP solver + result parsing:     ");
+    }
+  catch (GeneralError& e)
+    {
+      std::stringstream errstr;
+
+      // get exit code of process
+      if (retcode == -1)
+	{
+	  retcode = proc.close();
+	}
+
+      errstr << proc.path() << " (exitcode = " << retcode << "): " + e.getErrorMsg();
+
+      throw FatalError(errstr.str());
+    }
+  catch (std::exception& e)
+    {
+      throw FatalError(proc.path() + ": " + e.what());
+    }
+
+  // check for errors
+  if (retcode == 127)
+    {
+      throw FatalError("LP solver command `" + proc.path() + "´ not found!");
+    }
+  else if (retcode != 0) // other problem
+    {
+      std::stringstream errstr;
+
+      errstr << "LP solver `" << proc.path() << "´ bailed out with exitcode " << retcode << ": "
+	     << "re-run dlvhex with `strace -f´.";
+
+      throw FatalError(errstr.str());
+    }
+}
+
 
 DLVHEX_NAMESPACE_END
 
-/* vim: set noet sw=4 ts=4 tw=80: */
-
-
+/* vim: set noet sw=4 ts=8 tw=80: */
 // Local Variables:
 // mode: C++
 // End:
