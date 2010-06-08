e6461bb94f38ee6a1fbd39fd181e93af76333e52
Thomas Krennwallner <tkren@kr.tuwien.ac.at>
2010-06-08 06:30:02 +0000

    Add sanity checks for the autotool commands.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,42 @@
 #!/bin/sh
+#
+# Thomas Krennwallner <tkren@kr.tuwien.ac.at>
+#
+# 1. check for libtool, autoconf, automake, pkg-config
+# 2. run libtoolize and autoreconf to create libltdl, configure, and Makefile.in's
+#
 
 LT=`which libtoolize`
+GLT=`which glibtoolize`
 
 if [ -x "$LT" ]; then
     LT=libtoolize
-else
+elif [ -x "$GLT" ]; then
     LT=glibtoolize
+else
+    echo "libtoolize: command not found. Please install GNU libtool, GNU autoconf, and GNU automake."
+    exit 1
+fi
+
+ARC=`which autoreconf`
+
+if [ ! -x "$ARC" ]; then
+    echo "autoreconf: command not found. Please install GNU autoconf and GNU automake."
+    exit 1
+fi
+
+AM=`which automake`
+
+if [ ! -x "$AM" ]; then
+    echo "automake: command not found. Please install GNU automake."
+    exit 1
+fi
+
+PC=`which pkg-config`
+
+if [ ! -x "$PC" ]; then
+    echo "pkg-config: command not found. Please install pkg-config."
+    exit 1
 fi
 
 # copy libltdl into working repository
