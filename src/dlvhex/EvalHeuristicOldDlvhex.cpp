6dd9645c6f0a952b93961e44a42b2092766e3f63
Peter Schüller <ps@kr.tuwien.ac.at>
2010-11-25 10:41:12 +0000

    implementing old heuristics in a cleaner way and now without bugs

--- src/dlvhex/EvalHeuristicOldDlvhex.cpp
+++ src/dlvhex/EvalHeuristicOldDlvhex.cpp
@@ -35,6 +35,8 @@
 #include <boost/property_map/property_map.hpp>
 #include <boost/graph/properties.hpp>
 #include <boost/graph/depth_first_search.hpp>
+#include <boost/graph/reverse_graph.hpp>
+#include <boost/graph/topological_sort.hpp>
 
 DLVHEX_NAMESPACE_BEGIN
 
@@ -53,146 +55,132 @@ typedef ComponentGraph::ComponentIterator ComponentIterator;
 typedef ComponentGraph::SuccessorIterator SuccessorIterator;
 typedef ComponentGraph::PredecessorIterator PredecessorIterator;
 typedef std::set<Component> ComponentSet;
-
-namespace
-{
-  struct OriginalsDFSVisitor:
-    public boost::default_dfs_visitor
-  {
-    ComponentSet& origs;
-    OriginalsDFSVisitor(ComponentSet& origs): boost::default_dfs_visitor(), origs(origs) {}
-    OriginalsDFSVisitor(const OriginalsDFSVisitor& other): boost::default_dfs_visitor(), origs(other.origs) {}
-    void discover_vertex(Component comp, const ComponentGraph::Graph&)
-    {
-      LOG("discover " << comp);
-      origs.insert(comp);
-    }
-  };
-
-  struct OriginalsDFSTerminator
-  {
-    const ComponentGraph& cg;
-    const ComponentSet& origs;
-    OriginalsDFSTerminator(const ComponentGraph& cg, const ComponentSet& origs):
-      cg(cg), origs(origs) {}
-    bool operator()(Component comp, const ComponentGraph::Graph&)
-    {
-      // return true if vertex shall not be expanded
-      // -> return true for eatoms and for members of origs
-      bool hasEAtoms = (!cg.propsOf(comp).outerEatoms.empty());
-      bool isOrig = (origs.find(comp) != origs.end());
-      LOG("terminate?=" << hasEAtoms << "/" << isOrig << " @ " << comp);
-      return hasEAtoms || isOrig;
-    }
-  };
-}
+typedef std::list<Component> ComponentList;
+typedef std::vector<Component> ComponentVector;
 
 // old dlvhex approach:
-// 1) start at all roots (= components not depending on another component)
-// 2) mark from there using dfs/bfs until eatom hit (including eatom)
-// 3) collapse this into new component c
-// 4) take all nodes c depends on as new roots
-// 5) goto 2)
+// "calculate all that is calculateable" then go to next set of components and continue
+//
+// 1) do a topological sort of components not yet put into eval units
+// 2) go through components in order and mark "take" if
+//    * is an external component and depends only on prior eval units, or
+//    * is no external component and depends only on prior eval units or "take" components
+// 3) build eval unit from all marked as "take"
+// 4) restart
 void EvalHeuristicOldDlvhex::build()
 {
   ComponentGraph& compgraph = builder.getComponentGraph();
 
-	typedef std::list<Component> RootContainer;
-	RootContainer roots;
-	ComponentIterator cit, cit_end;
-	// find roots
-	for(boost::tie(cit, cit_end) = compgraph.getComponents();
-			cit != cit_end; ++cit)
-	{
-		SuccessorIterator sit, sit_end;
-		boost::tie(sit, sit_end) = compgraph.getProvides(*cit);
-		if( sit == sit_end )
-		{
-			roots.push_back(*cit);
-		}
-	}
+  // get internal compgraph
+  const ComponentGraph::Graph& igraph = compgraph.getInternalGraph();
 
-	// prepare dfs
+  //
+  // do topological sort
+	// (this sorting will remain stable, though elements may be removed)
+  //
 
-	// TODO think about how this can be made more efficient (below is the failsafe and slow version)
-	// (we could use two_bit_vector_property_map with custom IndexMap,
-	//  the index map knows the graph and can use properties of vertices to get some
-	//  index stored in the properties, the index is monotonically increasing and
-	//  managed by ComponentGraph.
-	//  we can preallocate a big property map (1.5*maxindex) -> no reallocations)
-
+  // prepare colormap
+  //
 	// we need a hash map, as component graph is no graph with vecS-storage
 	typedef boost::unordered_map<Component, boost::default_color_type> CompColorHashMap;
 	typedef boost::associative_property_map<CompColorHashMap> CompColorMap;
 	CompColorHashMap ccWhiteHashMap;
-
 	// fill white hash map
+  ComponentGraph::ComponentIterator cit, cit_end;
 	for(boost::tie(cit, cit_end) = compgraph.getComponents();
 			cit != cit_end; ++cit)
 	{
-		//boost::put(ccWhiteHashMap, *cit, boost::white_color);
 		ccWhiteHashMap[*cit] = boost::white_color;
 	}
 
-  // collect all created components here
-  // (we need to create them in the eval graph in the exact reverse order!)
-  std::list<Component> finishedComponents;
+  // do it
+  ComponentList opencomps;
+  std::back_insert_iterator<ComponentList> compinserter(opencomps);
+  CompColorHashMap ccHashMap(ccWhiteHashMap);
+  boost::topological_sort(
+      compgraph.getInternalGraph(),
+      compinserter,
+      boost::color_map(CompColorMap(ccHashMap)));
 
-	do
-	{
-		// step 2
-		LOG("starting step 2 of collapsing with roots " << printrange(roots));
+  ComponentList finishedcomps;
+  ComponentSet finishedcompsSet;
 
-		// components to collapse
-		typedef std::set<Component> ComponentSet;
-		ComponentSet originals;
+  do
+  {
+		LOG("creating new eval unit:");
+    LOG("open =     " << printrange(opencomps));
+    LOG("finished = " << printrange(finishedcomps));
 
-		// do dfs:
-		// from each root
-		// up to and including first eatom
-		// add nodes to originals
-		// if hitting an original, do not continue (pointless)
-
-		for(RootContainer::const_iterator itr = roots.begin();
-				itr != roots.end(); ++itr)
-		{
-      ComponentSet neworiginals;
-      OriginalsDFSVisitor dfs_vis(neworiginals);
-			CompColorHashMap ccHashMap(ccWhiteHashMap);
-			//CompColorMap ccMap(ccHashMap);
-			LOG("doing dfs visit for root " << *itr);
-			boost::depth_first_visit(
-					compgraph.getInternalGraph(),
-					*itr, 
-					dfs_vis,
-					CompColorMap(ccHashMap),
-					OriginalsDFSTerminator(compgraph, originals));
-      originals.insert(neworiginals.begin(), neworiginals.end());
-      neworiginals.clear();
-			LOG("dfs visit terminated: originals = " << printrange(originals));
-		}
-
-    // collapse originals into new component
-		Component newcomp = compgraph.collapseComponents(originals);
-    LOG("collapsing " << printrange(originals) << " yielded component " << newcomp);
-
-    // register as finished
-    finishedComponents.push_back(newcomp);
-
-    // calculate new roots (= all successors of new component)
-    roots.clear();
-    PredecessorIterator pit, pit_end;
-    for(boost::tie(pit, pit_end) = compgraph.getDependencies(newcomp);
-        pit != pit_end; ++pit)
+    ComponentSet markedcomps;
+    for(ComponentList::const_iterator it = opencomps.begin();
+        it != opencomps.end(); ++it)
     {
-			roots.push_back(compgraph.targetOf(*pit));
-		}
-	}
-	while( !roots.empty() );
+      Component comp = *it;
+      bool extcomp = !compgraph.propsOf(comp).outerEatoms.empty();
+      LOG("comp " << comp << " is " << (extcomp?"":"not ") << "external");
 
-  std::list<Component>::const_reverse_iterator it;
-  for(it = finishedComponents.rbegin();
-      it != finishedComponents.rend(); ++it)
+      // check dependencies
+      bool mark = true;
+      ComponentGraph::PredecessorIterator pit, pit_end;
+      for(boost::tie(pit, pit_end) = compgraph.getDependencies(comp);
+          pit != pit_end; ++pit)
+      {
+        Component dependsOn = compgraph.targetOf(*pit);
+        if( extcomp )
+        {
+          if( finishedcompsSet.find(dependsOn) == finishedcompsSet.end() )
+          {
+            // this is an external component and it depends on something not yet finished
+            mark = false;
+            break;
+          }
+        }
+        else
+        {
+          if( finishedcompsSet.find(dependsOn) == finishedcompsSet.end() &&
+              markedcomps.find(dependsOn) == markedcomps.end() )
+          {
+            // this is not an external component and it depends on something not yet finished or marked
+            mark = false;
+            break;
+          }
+        }
+      } // go through dependencies of each component
+      LOG("comp " << comp << " is " << (mark?"":"not ") << "marked for this eval unit");
+
+      if( mark )
+        markedcomps.insert(comp);
+    } // go through all components in order and determine marking for each of them
+
+    LOG("marked = " << printrange(markedcomps));
+
+    // collapse marked into new component
+		Component newcomp = compgraph.collapseComponents(markedcomps);
+    LOG("collapsing marked yielded component " << newcomp);
+
+    // remove marked from opencomps
+    for(ComponentList::iterator it = opencomps.begin();
+        it != opencomps.end();)
+    {
+      if( markedcomps.find(*it) != markedcomps.end() )
+      {
+        ComponentList::iterator backup = it;
+        it++; // may now be end() !
+        opencomps.erase(backup);
+        continue; // does not increment, first checks if end
+      }
+      // do it here, so that continue; does not increment
+      it++;
+    }
+
+    // add newcomp to finished set and list
+    finishedcomps.push_back(newcomp);
+    finishedcompsSet.insert(newcomp);
+  }
+  while(!opencomps.empty());
+
+  for(ComponentList::const_iterator it = finishedcomps.begin();
+      it != finishedcomps.end(); ++it)
   {
     // build eval unit
     EvalGraphBuilder::EvalUnit u = builder.createEvalUnit(*it);
