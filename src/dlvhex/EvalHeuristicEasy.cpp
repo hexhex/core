8dedfcbe2e56a1348c84c054f59f0bc234da8e76
Peter Schüller <ps@kr.tuwien.ac.at>
2010-11-26 12:53:18 +0000

    improving easy eval heuristic

--- src/dlvhex/EvalHeuristicEasy.cpp
+++ src/dlvhex/EvalHeuristicEasy.cpp
@@ -51,9 +51,34 @@ typedef ComponentGraph::Component Component;
 typedef ComponentGraph::ComponentIterator ComponentIterator;
 typedef std::vector<Component> ComponentContainer;
 
-// trivial strategy:
-// do a topological sort of the tree
-// build eval units in that order
+template<typename ComponentGraph, typename Sequence>
+void topologicalSortOfComponents(const ComponentGraph& compgraph, Sequence& comps)
+{
+  // we need a hash map, as component graph is no graph with vecS-storage
+  //
+  typedef boost::unordered_map<Component, boost::default_color_type> CompColorHashMap;
+  typedef boost::associative_property_map<CompColorHashMap> CompColorMap;
+  CompColorHashMap ccWhiteHashMap;
+  // fill white hash map
+  ComponentIterator cit, cit_end;
+  for(boost::tie(cit, cit_end) = compgraph.getComponents();
+      cit != cit_end; ++cit)
+  {
+    //boost::put(ccWhiteHashMap, *cit, boost::white_color);
+    ccWhiteHashMap[*cit] = boost::white_color;
+  }
+  CompColorHashMap ccHashMap(ccWhiteHashMap);
+
+  //
+  // do topological sort
+  //
+  std::back_insert_iterator<Sequence> compinserter(comps);
+  boost::topological_sort(
+      compgraph.getInternalGraph(),
+      compinserter,
+      boost::color_map(CompColorMap(ccHashMap)));
+}
+
 void EvalHeuristicEasy::build()
 {
   ComponentGraph& compgraph = builder.getComponentGraph();
@@ -61,11 +86,16 @@ void EvalHeuristicEasy::build()
   typedef ComponentGraph::ComponentSet ComponentSet;
 
   //
-  // forall external atoms:
-  // merge with rules that contain them if they don't depend on something else
-  // TODO relax that condition
+  // forall external components e:
+  // merge with all rules that 
+  // * do not contain external atoms
+  // * depend on e 
+  // * do not depend on something e does not (transitively) depend on
   //
   {
+    //ComponentContainer sortedcomps;
+    //topologicalSortOfComponents(compgraph, sortedcomponents);
+
     ComponentIterator cit;
     for(cit = compgraph.getComponents().first; // do not use boost::tie here! the container is modified in the loop!
         cit != compgraph.getComponents().second; ++cit)
@@ -76,12 +106,22 @@ void EvalHeuristicEasy::build()
 
       LOG("checking whether to collapse external component " << comp);
 
+      // get predecessors (we could do transitive closure here)
+      ComponentSet preds;
+      ComponentGraph::PredecessorIterator pit, pit_end;
+      for(boost::tie(pit, pit_end) = compgraph.getDependencies(comp);
+          pit != pit_end; ++pit)
+      {
+        preds.insert(compgraph.targetOf(*pit));
+      }
+      // insert this component
+      preds.insert(comp);
+
       // get successors
       ComponentSet collapse;
       ComponentGraph::SuccessorIterator sit, sit_end;
       for(boost::tie(sit, sit_end) = compgraph.getProvides(comp);
-          sit != sit_end;
-          ++sit)
+          sit != sit_end; ++sit)
       {
         Component succ = compgraph.sourceOf(*sit);
 
@@ -92,22 +132,23 @@ void EvalHeuristicEasy::build()
         LOG("found successor " << succ);
 
         ComponentGraph::PredecessorIterator pit, pit_end;
-        boost::tie(pit, pit_end) = compgraph.getDependencies(succ);
         bool good = true;
-        assert(pit != pit_end);
-        if( compgraph.targetOf(*pit) != comp )
+        for(boost::tie(pit, pit_end) = compgraph.getDependencies(succ);
+            pit != pit_end; ++pit)
         {
-          LOG("successor bad as it depends on other node " << compgraph.targetOf(*pit));
-          good = false;
-        }
-        pit++;
-        if( pit != pit_end )
-        {
-          good = false;
-          LOG("successor bad as it depends on more nodes");
+          Component dependson = compgraph.targetOf(*pit);
+          if( preds.find(dependson) == preds.end() )
+          {
+            LOG("successor bad as it depends on other node " << dependson);
+            good = false;
+            break;
+          }
         }
         if( good )
+        {
           collapse.insert(succ);
+          preds.insert(succ);
+        }
       }
 
       // collapse if not nonempty
@@ -125,8 +166,7 @@ void EvalHeuristicEasy::build()
 
   //
   // forall components with only inner rules:
-  // if they contain a guess, merge with children that do not contain a guess and no eatoms and do not depend on anything else
-  // TODO relax that condition
+  // merge with children that are no eatoms and do not depend on anything else
   //
   {
     ComponentIterator cit = compgraph.getComponents().first;
@@ -196,41 +236,40 @@ void EvalHeuristicEasy::build()
   }
 
   //
+  // forall components with only inner constraints:
+  // merge with all other constraint-only components
+  //
+  {
+    ComponentSet collapse;
+
+    ComponentIterator cit;
+    for(cit = compgraph.getComponents().first; // do not use boost::tie here! the container is modified in the loop!
+        cit != compgraph.getComponents().second; ++cit)
+    {
+      Component comp = *cit;
+      if( compgraph.propsOf(comp).outerEatoms.empty() &&
+          compgraph.propsOf(comp).innerRules.empty() )
+        collapse.insert(comp);
+    }
+
+    if( !collapse.empty() )
+    {
+      // collapse! (decreases graph size)
+      LOG("collapsing constraint-only nodes " << printrange(collapse));
+      Component c = compgraph.collapseComponents(collapse);
+    }
+  }
+
+  //
   // create eval units using topological sort
   //
-
+  ComponentContainer sortedcomps;
+  topologicalSortOfComponents(compgraph, sortedcomps);
+  for(ComponentContainer::const_iterator it = sortedcomps.begin();
+      it != sortedcomps.end(); ++it)
   {
-    // we need a hash map, as component graph is no graph with vecS-storage
-    //
-    typedef boost::unordered_map<Component, boost::default_color_type> CompColorHashMap;
-    typedef boost::associative_property_map<CompColorHashMap> CompColorMap;
-    CompColorHashMap ccWhiteHashMap;
-    // fill white hash map
-    ComponentIterator cit, cit_end;
-    for(boost::tie(cit, cit_end) = compgraph.getComponents();
-        cit != cit_end; ++cit)
-    {
-      //boost::put(ccWhiteHashMap, *cit, boost::white_color);
-      ccWhiteHashMap[*cit] = boost::white_color;
-    }
-    CompColorHashMap ccHashMap(ccWhiteHashMap);
-
-    //
-    // do topological sort
-    //
-    ComponentContainer comps;
-    std::back_insert_iterator<ComponentContainer> compinserter(comps);
-    boost::topological_sort(
-        compgraph.getInternalGraph(),
-        compinserter,
-        boost::color_map(CompColorMap(ccHashMap)));
-
-    for(ComponentContainer::const_iterator it = comps.begin();
-        it != comps.end(); ++it)
-    {
-      EvalGraphBuilder::EvalUnit u = builder.createEvalUnit(*it);
-      LOG("component " << *it << " became eval unit " << u);
-    }
+    EvalGraphBuilder::EvalUnit u = builder.createEvalUnit(*it);
+    LOG("component " << *it << " became eval unit " << u);
   }
 }
 
