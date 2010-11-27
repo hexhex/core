9ffbb8e12068d2dcdb0830d55560c8253219a6e6
Peter Schüller <ps@kr.tuwien.ac.at>
2010-11-27 17:40:40 +0000

    benchmark bugfix

--- testsuite/benchgen/revsel1.cpp
+++ testsuite/benchgen/revsel1.cpp
@@ -5,11 +5,6 @@
 
 #include "dlvhex/Logger.hpp"
 
-#if 0
-#include <gmp.h>
-#include <gmpxx.h>
-#endif
-
 #include <boost/foreach.hpp>
 
 #include <sstream>
@@ -141,7 +136,7 @@ int main(int ac,char** av)
     ("sconflicts,b", po::value<unsigned>(&config.sconflicts)->required(),
       "percentage of shared reviewers with less conflicts")
     ("noext,n", po::value<unsigned>(&config.noext)->default_value(0),
-      "do not output external atoms")
+      "percentage of non-external atoms for conflicts")
   ;
   po::variables_map vm;
   po::store(po::parse_command_line(ac, av, desc), vm);
@@ -156,14 +151,13 @@ int main(int ac,char** av)
 
   RandomNumbers random;
 
-  std::stringstream prelude;
+  std::ostream& o = std::cout;
+
   StringVector tracksyms;
   StringVector papersyms;
   StringVector urefereesyms;
   StringVector srefereesyms;
   {
-    std::ostream& o = prelude;
-
     // create symbols (shared across strata)
     genSyms("track", config.tracks, tracksyms);
     genSyms("paper", config.tracks*config.papers, papersyms);
@@ -200,33 +194,8 @@ int main(int ac,char** av)
     }
   }
 
-  #if 0
-  mpz_t guessspace;
+  // create conflicts
   {
-    mpz_t oneguessspace;
-    mpz_init(oneguessspace);
-    mpz_bin_uiui(oneguessspace, config.ureferees+config.sreferees, 2); 
-
-    mpz_init_set_ui(guessspace,1);
-    for(unsigned u = 0; u < config.papers; ++u)
-    {
-      mpz_mul(guessspace, guessspace, oneguessspace);
-    }
-  }
-  LOG("guessing space for each track = " << mpz_class(guessspace).get_str());
-
-  mpz_t bestdif;
-  mpz_init_set_ui(bestdif, config.models);
-
-  do
-  {
-    LOG("starting iteration");
-    mpz_t models;
-    mpz_init_set_ui(models,1);
-  #endif
-
-    std::stringstream o;
-
     // create track-local conflicts
     for(unsigned t = 0; t < config.tracks; ++t)
     {
@@ -277,7 +246,8 @@ int main(int ac,char** av)
       if( (r*100/config.sreferees) < config.sconflicts )
       {
         unsigned removed = 0;
-        while(removed < 3 )
+        unsigned timeout = 100;
+        while( removed < 3  && (timeout-- != 0) )
         {
           unsigned remove = random.getInRange(0, config.tracks*config.papers-1);
           if( conflict.count(remove) > 0 )
@@ -294,7 +264,10 @@ int main(int ac,char** av)
         o << "conflict(" << papersyms[pap] << "," << srefereesyms[r] << ")." << std::endl;
       }
     }
+  }
 
+  // create rules
+  {
     for(unsigned t = 0; t < config.tracks; ++t)
     {
       o << "assign(" << tracksyms[t] << ",P,R) v nassign(" << tracksyms[t] << ",P,R) :- track_paper(" << tracksyms[t] << ",P), track_referee(" << tracksyms[t] << ",R)." << std::endl;
@@ -309,283 +282,31 @@ int main(int ac,char** av)
       o << ":- assign(" << tracksyms[t] << ",P1,R), assign(" << tracksyms[t] << ",P2,R), assign(" << tracksyms[t] << ",P3,R), P1 != P2, P1 != P3, P2 != P3." << std::endl;
 
       // conflicts (local)
-      if( config.noext == 1 )
+      if( random.getInRange(0,100) < config.noext )
+      {
         o << ":- assign(" << tracksyms[t] << ",P,R), conflict(P,R)." << std::endl;
+      }
       else
-        o << ":- assign(" << tracksyms[t] << ",P,R), &gen2[conflict,P,R]()." << std::endl;
-
-      //mpz_mul(models, models, guessspace);
+      {
+        o << ":- assign(" << tracksyms[t] << ",P,R), conflict(P,R). % REMOVEFORHEX" << std::endl;
+        o << ":- assign(" << tracksyms[t] << ",P,R), &gen2[conflict,P,R](). % ONLYFORHEX" << std::endl;
+      }
     }
 
     // no more than 2 assignments per reviewer (global)
     o << ":- assign(T,P1,R), assign(T,P2,R), assign(T,P3,R), P1 != P2, P1 != P3, P2 != P3." << std::endl;
 
     // conflicts (global)
-    if( config.noext == 1 )
-      o << ":- assign(T,P,R), conflict(P,R)." << std::endl;
-    else
-      o << ":- assign(T,P,R), &gen2[conflict,P,R]()." << std::endl;
-
-    //LOG("models so far = " << mpz_class(models).get_str());
-
-    // add conflicts
-
-
-
-    #if 0
-    // calculate difference to reference model count
-    mpz_t dif;
-    mpz_init(dif);
-    mpz_sub_ui(dif, models, config.models);
-    mpz_abs(dif, dif);
-    LOG("difference here is " << mpz_class(dif).get_str() << "!");
-
-    if( mpz_cmp(dif, bestdif) < 0 )
+    if( random.getInRange(0,100) < config.noext )
     {
-      mpz_set(bestdif, dif);
-      LOG("this is new best dif! " << mpz_class(dif).get_str());
-
-      if( mpz_cmp_ui(bestdif, config.tolerance) < 0 )
-      {
-        LOG("this is below tolerance!");
-        #endif
-        std::cout << prelude.str();
-        std::cout << o.str();
-        #if 0
-        break;
-      }
+      o << ":- assign(T,P,R), conflict(P,R)." << std::endl;
+    }
+    else
+    {
+      o << ":- assign(T,P,R), conflict(P,R). % REMOVEFORHEX" << std::endl;
+      o << ":- assign(T,P,R), &gen2[conflict,P,R](). % ONLYFORHEX" << std::endl;
     }
   }
-  while(true);
-  #endif
-
-
-
-
-
-
-
-
-
-
-
-
-  #if 0
-
-      StringVector guessSyms(symbols);
-      randomizeRange(random, guessSyms);
-      for(unsigned g = 0; g < config.g; ++g)
-      {
-        o << in(tow,guessSyms[2*g]) << " :- not " << in(tow,guessSyms[2*g+1]) << "." << std::endl;
-        o << in(tow,guessSyms[2*g+1]) << " :- not " << in(tow,guessSyms[2*g]) << "." << std::endl;
-      }
-  }
-
-  // ic
-  for(unsigned tow = 0; tow < config.t; ++tow)
-  {
-      // choose build ic distinct constraints
-      // (without distinct we may not kill enough,
-      // this way we have more fine-grained control over the number of models)
-      std::set< std::set<unsigned> > constraints;
-      while(constraints.size() != config.ic)
-      {
-        // choose 2 distinct symbols to exclude
-        // (without distinct we may kill a lot with few constraints,
-        // this way we have more fine-grained control over the number of models)
-
-        unsigned s1 = random.getInRange(0, nsymbols-1);
-        unsigned s2 = s1;
-        while( s2 == s1 )
-          s2 = random.getInRange(0, nsymbols-1);
-        unsigned s3 = s1;
-        while( s3 == s1 || s3 == s2 )
-          s3 = random.getInRange(0, nsymbols-1);
-
-        std::set<unsigned> cstr;
-        cstr.insert(s1);
-        cstr.insert(s2);
-        cstr.insert(s3);
-        constraints.insert(cstr);
-      }
-
-      // build ic constraints
-      for(std::set< std::set<unsigned> >::const_iterator it = constraints.begin();
-          it != constraints.end(); ++it)
-      {
-        o << ":- ";
-        std::set<unsigned>::const_iterator its = it->begin();
-        o << in(tow,symbols[*its]) << ",";
-        its++;
-        assert(its != it->end());
-        o << in(tow,symbols[*its]) << ",";
-        its++;
-        assert(its != it->end());
-        o << in(tow,symbols[*its]) << "." << std::endl;
-        its++;
-        assert(its == it->end());
-      }
-  }
-
-  // ec
-  for(unsigned tow = 0; tow < config.t; ++tow)
-  {
-      // choose build ec distinct constraints
-      // (without distinct we may not kill enough,
-      // this way we have more fine-grained control over the number of models)
-      std::set< std::set<unsigned> > constraints;
-      while(constraints.size() != config.ec)
-      {
-        // choose 2 distinct symbols to exclude
-        // (without distinct we may kill a lot with few constraints,
-        // this way we have more fine-grained control over the number of models)
-
-        unsigned s1 = random.getInRange(0, nsymbols-1);
-        unsigned s2 = s1;
-        while( s2 == s1 )
-          s2 = random.getInRange(0, nsymbols-1);
-
-        std::set<unsigned> cstr;
-        cstr.insert(s1);
-        cstr.insert(s2);
-        constraints.insert(cstr);
-      }
-
-      // build ic constraints
-      for(std::set< std::set<unsigned> >::const_iterator it = constraints.begin();
-          it != constraints.end(); ++it)
-      {
-        o << ":- ";
-        o <<
-          "&above[" << inPred(tow) << "," << symbols[random.getInRange(0, nsymbols-1)] << "]" <<
-          "(" << symbols[random.getInRange(0, nsymbols-1)] << "),";
-        std::set<unsigned>::const_iterator its = it->begin();
-        assert(its != it->end());
-        o << in(tow,symbols[*its]) << ",";
-        its++;
-        assert(its != it->end());
-        o << in(tow,symbols[*its]) << "." << std::endl;
-        its++;
-        assert(its == it->end());
-      }
-  }
-  
-  // gic
-  {
-      if( config.gic != 0 && config.t < 3 )
-        throw std::runtime_error("for gic != 0, t must be >= 3");
-
-      // choose gic distinct constraints
-      // (without distinct we may not kill enough,
-      // this way we have more fine-grained control over the number of models)
-      std::set< std::vector<unsigned> > constraints;
-      while(constraints.size() != config.gic)
-      {
-        // choose 3 distinct symbols in 3 distinct towers to exclude
-        // (without distinct we may kill a lot with few constraints,
-        // this way we have more fine-grained control over the number of models)
-
-        unsigned t1 = random.getInRange(0, config.t-1);
-        unsigned t2 = t1;
-        while( t2 == t1 )
-          t2 = random.getInRange(0, config.t-1);
-        unsigned t3 = t1;
-        while( t3 == t1 || t3 == t2 )
-          t3 = random.getInRange(0, config.t-1);
-
-        unsigned s1 = random.getInRange(0, nsymbols-1);
-        unsigned s2 = s1;
-        while( s2 == s1 )
-          s2 = random.getInRange(0, nsymbols-1);
-        unsigned s3 = s1;
-        while( s3 == s1 || s3 == s2 )
-          s3 = random.getInRange(0, nsymbols-1);
-
-        std::vector<unsigned> cstr;
-        cstr.push_back(t1);
-        cstr.push_back(s1);
-        cstr.push_back(t2);
-        cstr.push_back(s2);
-        cstr.push_back(t3);
-        cstr.push_back(s3);
-        constraints.insert(cstr);
-      }
-
-      // build gic constraints
-      for(std::set< std::vector<unsigned> >::const_iterator it = constraints.begin();
-          it != constraints.end(); ++it)
-      {
-        const std::vector<unsigned>& vec = *it;
-        assert(vec.size() == 6);
-        o << ":- ";
-        o << in(vec[0],symbols[vec[1]]) << ",";
-        o << in(vec[2],symbols[vec[3]]) << ",";
-        o << in(vec[4],symbols[vec[5]]) << "." << std::endl;
-      }
-  }
-
-  // gec
-  {
-      if( config.gec != 0 && config.t < 3 )
-        throw std::runtime_error("for gec != 0, t must be >= 3");
-
-      // choose gec distinct constraints
-      // (without distinct we may not kill enough,
-      // this way we have more fine-grained control over the number of models)
-      std::set< std::vector<unsigned> > constraints;
-      while(constraints.size() != config.gec)
-      {
-        // choose 3 distinct symbols in 3 distinct towers to exclude
-        // (without distinct we may kill a lot with few constraints,
-        // this way we have more fine-grained control over the number of models)
-
-        unsigned t1 = random.getInRange(0, config.t-1);
-        unsigned t2 = t1;
-        while( t2 == t1 )
-          t2 = random.getInRange(0, config.t-1);
-        unsigned t3 = t1;
-        while( t3 == t1 || t3 == t2 )
-          t3 = random.getInRange(0, config.t-1);
-
-        unsigned s1 = random.getInRange(0, nsymbols-1);
-        unsigned s2 = s1;
-        while( s2 == s1 )
-          s2 = random.getInRange(0, nsymbols-1);
-        unsigned s3 = s1;
-        while( s3 == s1 || s3 == s2 )
-          s3 = random.getInRange(0, nsymbols-1);
-
-        std::vector<unsigned> cstr;
-        cstr.push_back(t1);
-        cstr.push_back(s1);
-        cstr.push_back(t2);
-        cstr.push_back(s2);
-        cstr.push_back(t3);
-        cstr.push_back(s3);
-        constraints.insert(cstr);
-      }
-
-      // build gec constraints
-      for(std::set< std::vector<unsigned> >::const_iterator it = constraints.begin();
-          it != constraints.end(); ++it)
-      {
-        const std::vector<unsigned>& vec = *it;
-        assert(vec.size() == 6);
-        o << ":- ";
-        o <<
-          "&above[" << inPred(vec[0]) << "," << symbols[vec[1]] << "]" <<
-          "(" << symbols[vec[1]] << "),";
-        o <<
-          "&above[" << inPred(vec[2]) << "," << symbols[vec[3]] << "]" <<
-          "(" << symbols[vec[3]] << "),";
-        o <<
-          "&above[" << inPred(vec[4]) << "," << symbols[vec[5]] << "]" <<
-          "(" << symbols[vec[5]] << ")." << std::endl;
-      }
-  }
-
-
-  #endif
 
   return 0;
 
