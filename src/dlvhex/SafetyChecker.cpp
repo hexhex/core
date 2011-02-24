daba5f9646f504dbf48c1d809b14c1b07449670c
Peter Schüller <ps@kr.tuwien.ac.at>
2011-02-24 14:44:28 +0000

    implemented strong safety checker

--- src/dlvhex/SafetyChecker.cpp
+++ src/dlvhex/SafetyChecker.cpp
@@ -384,158 +384,233 @@ StrongSafetyChecker::~StrongSafetyChecker()
 {
 }
 
+// testing for strong safety:
+//
+// A rule is strongly safe, if
+// * it is safe and
+// * if an external atom in the rule is part of a cycle, each variable in
+//   its output list occurs in a positive atom in the body, which does not
+//   belong to the cycle.
+//
+// this is implemented as
+// for each component c
+// A) check if any external atom has output variables, if no exit with success
+// B) get all rule heads in c
+// C) for each rule r in c
+//   a) for each external atom e in the body of r
+//     1) for each output variable of e
+//        if e is part of a positive body atom of r
+//        and this positive body atom of r does not unify with any rule head in c
+//        then e is safe
+//     2) if any output variable of e is not safe, rule r is not strongly safe
 void
 StrongSafetyChecker::operator() () const throw (SyntaxError)
 {
-  #if 0
-  if (Globals::Instance()->doVerbose(Globals::SAFETY_ANALYSIS))
-    {
-      Globals::Instance()->getVerboseStream() << std::endl << "Checking for strong rule safety." << std::endl;
-    }
+  LOG_SCOPE(ANALYZE,"strongsafety",false);
+	LOG(ANALYZE,"=strong safety checker");
 
-  //
-  // testing for strong safety:
-  //
-  // A rule is strongly safe, if
-  // * it is safe and
-  // * if an external atom in the rule is part of a cycle, each variable in
-  //   its output list occurs in a positive atom in the body, which does not
-  //   belong to the cycle.
-  //
-	// this is implemented as
-	// for each component c
-	// A) get all rule heads in c
-	// B) for each rule r in c
-	//   a) for each external atom e in the body of r
-	//     1) for each output variable of e
-	//        if e is part of a positive body atom of r
-	//        and this positive body atom of r does not unify with any rule head in c
-	//        then e is safe
-	//     2) if any output variable of e is not safe, rule r is not strongly safe
+  RegistryPtr reg = ctx.registry();
+  assert(!!reg);
 
-  //
-  // go through all program components
-  // (a ProgramComponent is a SCC with external atom!)
-  //
-  const std::vector<Component*>& components = dg.getComponents();
+	// at this point we may (and do) assume that all rules are safe
 
-  for (std::vector<Component*>::const_iterator compit = components.begin();
-       compit != components.end();
-       ++compit)
-    {
-      if (typeid(**compit) == typeid(ProgramComponent))
-        {
-	  //
-	  // go through all rules of this component
-	  //
-	  ProgramComponent* progcomp = static_cast<ProgramComponent*>(*compit);
+	// we also assume that we have a component graph
+	assert(!!ctx.compgraph);
+	ComponentGraph::ComponentIterator itc, itc_end;
+	for(boost::tie(itc, itc_end) = ctx.compgraph->getComponents();
+			itc != itc_end; ++itc)
+	{
+		const ComponentGraph::ComponentInfo& ci = ctx.compgraph->propsOf(*itc);
 
-	  const Program& rules = progcomp->getBottom();
+		// ignore components without inner eatoms
+		// (they are automatically strongly safe)
+		if( ci.innerEatoms.empty() )
+			continue;
 
-	  for (Program::const_iterator ruleit = rules.begin(); ruleit != rules.end(); ++ruleit)
-            {
-	      const RuleBody_t& body = (*ruleit)->getBody();
-	      const std::vector<ExternalAtom*>& exts = (*ruleit)->getExternalAtoms();
-	      
-	      //
-	      // for this rule: go through all ext-atoms
-	      //
-	      for (std::vector<ExternalAtom*>::const_iterator extit = exts.begin(); extit != exts.end(); ++extit)
-                {
-		  //
-		  // is this atom also in the component?
-		  // (not all the atoms in the bottom of a component are also
-		  // in the component themselves!)
-		  //
-		  if (!progcomp->isInComponent(*extit))
-		    {
-		      continue;
-		    }
-
-		  //
-		  // ok, this external atom is in the cycle of the component:
-		  // now we have to check if each of its output arguments is
-		  // strongly safe, i.e., if it occurs in another atom in the
-		  // body, which is NOT part of the cycle
-		  //
-		  const Tuple& output = (*extit)->getArguments();
-
-		  //
-		  // look at all terms in its output list
-		  //
-		  for (Tuple::const_iterator outterm = output.begin();
-		       outterm != output.end();
-		       ++outterm)
-                    {
-		      // only check variable output arguments
-		      ///@todo anon vars can't be expansion safe
-		      if (outterm->isVariable() || outterm->isAnon()) 
+		// check if any external atom has output variables
+		bool needToCheck = false;
+		BOOST_FOREACH(ID eaid, ci.innerEatoms)
+		{
+			const ExternalAtom& eatom = reg->eatoms.getByID(eaid);
+			BOOST_FOREACH(ID otid, eatom.tuple)
 			{
-			  bool outIsSafe = false;
-
-			  for (RuleBody_t::const_iterator bodylit = body.begin();
-			       bodylit != body.end(); ++bodylit)
-			    {
-			      //
-			      // only look at atoms that are not part of the
-			      // component!
-			      // and only look at ordinary ones;
-			      // external atoms and builtins do not make a variable safe!
-			      //
-			      const Atom& at = *(*bodylit)->getAtom();
-			      
-			      if (typeid(at) == typeid(Atom))
+				if( otid.isVariableTerm() )
 				{
-				  if (!(*compit)->isInComponent((*bodylit)->getAtom().get()))
-				    {
-				      //
-				      // the arguments of this atom are safe
-				      //
-				      const Tuple& safeargs = at.getArguments();
-				      
-				      //
-				      // now see if the current
-				      // extatom-output-argument is one of those
-				      // safe vars
-				      //
-				      for (Tuple::const_iterator safeterm = safeargs.begin();
-					   safeterm != safeargs.end();
-					   ++safeterm)
-					{
-					  if (safeterm->isVariable() && 
-					      *safeterm == *outterm)
-					    {
-					      outIsSafe = true;
-					      break;
-					    }
-					}
-				    }
+					needToCheck = true;
+					break;
 				}
-			    }
-			  
-			  // we have looked at all the body-atoms, but
-			  // couldn't find an atom, which make this output
-			  // variable safe
-			  if (!outIsSafe)
-			    {
-			      std::stringstream s;
-			      s << "rule not expansion-safe: " << **ruleit;
-			      throw SyntaxError(s.str());
-			    }
 			}
-                    }
-                }
-	      
-	      if (Globals::Instance()->doVerbose(Globals::SAFETY_ANALYSIS))
-                {
-		  Globals::Instance()->getVerboseStream() << "Rule " << **ruleit
-							  << " is expansion-safe." << std::endl;
-                }
-	      
-            } // rules-loop end
-        }
-    }
-  #endif
+			if( needToCheck )
+				break;
+		}
+
+		if( !needToCheck )
+		{
+			DBGLOG(DBG,"no need to check strong safety "
+					"as there are no outputs in internal eatoms " <<
+					printManyToString<RawPrinter>(ci.innerEatoms, ", ", reg));
+			continue;
+		}
+		DBGLOG(DBG,"need to check strong safety in component " << *itc);
+
+		// get rule heads here
+		// here we store the full atom IDs (we need to unify, the predicate is not sufficient)
+		std::set<ID> headAtomIDs;
+		// we only consider inner rules (constraints have no heads)
+		BOOST_FOREACH(ID rid, ci.innerRules)
+		{
+			const Rule& rule = reg->rules.getByID(rid);
+			
+			BOOST_FOREACH(ID hid, rule.head)
+			{
+				if( !hid.isOrdinaryAtom() )
+				{
+					LOG(WARNING,"ignoring non-ordinary atom in rule head for strong safety checking: " <<
+							printToString<RawPrinter>(hid, reg));
+					continue;
+				}
+				headAtomIDs.insert(hid);
+			}
+		}
+
+		DBGLOG(DBG,"in component " <<  *itc <<
+				" got set of heads '" <<
+				printManyToString<RawPrinter>(
+					Tuple(headAtomIDs.begin(), headAtomIDs.end()), ", ", reg) << "'");
+
+		// now check output variables
+
+		// we again only consider inner rules (positive domain expansion feedback
+		// cannot happen through constraints as they cannot generate symbols)
+		BOOST_FOREACH(ID rid, ci.innerRules)
+		{
+			if( !rid.doesRuleContainExtatoms() )
+			{
+				DBGLOG(DBG,"skipping strong safety check for rule " <<
+						printToString<RawPrinter>(rid, reg) << " (no external atoms)");
+				continue;
+			}
+
+			const Rule& rule = reg->rules.getByID(rid);
+
+			DBGLOG(DBG,"now checking strong safety of rule " <<
+					printToString<RawPrinter>(rid, reg));
+
+			// find all variable outputs in all eatoms in this rule's body
+			std::set<ID> varsToCheck;
+			BOOST_FOREACH(ID lid, rule.body)
+			{
+				if( !lid.isExternalAtom() )
+					continue;
+
+				const ExternalAtom& eatom = reg->eatoms.getByID(lid);
+				BOOST_FOREACH(ID tid, eatom.tuple)
+				{
+					if( tid.isVariableTerm() )
+						varsToCheck.insert(tid);
+				}
+			}
+
+			#warning anonymous variables or variables that do not occur in any other atom should be automatically strongly safe...?
+			DBGLOG(DBG,"need to find component-external domain predicate "
+					"for variables {" << printManyToString<RawPrinter>(
+						Tuple(varsToCheck.begin(), varsToCheck.end()), ", ", reg) + "}");
+
+			// for each variable:
+			// if it is part of a positive body atom of r
+			// and this positive body atom of r does not unify with any rule head in c
+			// then e is safe
+			BOOST_FOREACH(ID vid, varsToCheck)
+			{
+				// check strong safety of variable vid
+				DBGLOG(DBG,"checking strong safety of variable " << 
+						printToString<RawPrinter>(vid,reg));
+
+				bool variableSafe = false;
+				BOOST_FOREACH(ID lid, rule.body)
+				{
+					// skip negative bodies
+					if( lid.isNaf() )
+						continue;
+
+					// skip external atoms,
+					// they could but cannot in general be assumed to limit the domain
+					// (and that's the reason we need to check strong safety)
+					if( lid.isExternalAtom() )
+						continue;
+
+					// skip non-ordinary atoms
+					#warning can we use aggregates to limit the domain for strong safety?
+					#warning can we use builtin atoms to limit the domain for strong safety?
+					if( lid.isAggregateAtom() ||
+							lid.isBuiltinAtom() )
+						continue;
+
+					assert(lid.isOrdinaryAtom());
+
+					// check if this body literal contains the variable
+					// and does not unify with any head
+					// (only then the variable is safe)
+					bool containsVariable = false;
+					const OrdinaryAtom& oatom = reg->lookupOrdinaryAtom(lid);
+					assert(!oatom.tuple.empty());
+					Tuple::const_iterator itv = oatom.tuple.begin();
+					itv++;
+					while( itv != oatom.tuple.end() )
+					{
+						if( *itv == vid )
+						{
+							containsVariable = true;
+							break;
+						}
+						itv++;
+					}
+
+					if( !containsVariable )
+					{
+						DBGLOG(DBG,"skipping body literal " <<
+								printToString<RawPrinter>(lid, reg) <<
+								" (does not contain variable)");
+						continue;
+					}
+
+					// oatom 'oatom' was retrieved using ID 'lid'
+					DBGLOG(DBG,"checking unifications of body literal " <<
+							printToString<RawPrinter>(lid, reg) << " with component rule heads");
+					bool doesNotUnify = true;
+					BOOST_FOREACH(ID hid, headAtomIDs)
+					{
+						DBGLOG(DBG,"checking against " <<
+								printToString<RawPrinter>(hid, reg));
+						assert(hid.isOrdinaryAtom());
+						const OrdinaryAtom& hoatom = reg->lookupOrdinaryAtom(hid);
+						if( oatom.unifiesWith(hoatom) )
+						{
+							DBGLOG(DBG,"unification successful "
+									"-> literal does not limit the domain");
+							doesNotUnify = false;
+							break;
+						}
+					}
+
+					if( doesNotUnify )
+					{
+						DBGLOG(DBG, "variable safe!");
+						variableSafe = true;
+						break;
+					}
+				}
+
+				if( !variableSafe )
+				{
+					throw SyntaxError("Rule is not strongly safe! "
+							" Variable " + printToString<RawPrinter>(vid,reg) +
+							" fails strong safety check in rule " +
+							printToString<RawPrinter>(rid, reg));
+				}
+			}
+		}
+	}
 }
 
 
