22b2775004c1bb253253233c6e1936379df56280
Tri Kurniawan Wijaya <trikurniawanwijaya@gmail.com>
2011-04-25 18:38:52 +0000

    Create file summary for each parameter setting.

---
+++ generator/summaryET.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+mainDir=$1		#main directory where all folder parameter setting are
+resultDir=$2
+rm -rf $2
+mkdir $2
+for dir in $mainDir/*; do 
+	if [ -d $dir ]; then
+		shortDir=${dir#$mainDir/}
+		mkdir $2/$shortDir
+		fileSummaryMI="$2/$shortDir/summary-MI-$shortDir.txt"
+		fileSummarySizeM="$2/$shortDir/summary-SizeM-$shortDir.txt"
+		fileSummaryASDLV="$2/$shortDir/summary-ASDLV-$shortDir.txt"
+		fileSummaryCallDLV="$2/$shortDir/summary-CallDLV-$shortDir.txt"
+		fileSummaryTime="$2/$shortDir/summary-Time-$shortDir.txt"
+		echo "" > $fileSummaryMI
+		echo "" > $fileSummarySizeM
+		echo "" > $fileSummaryASDLV
+		echo "" > $fileSummaryCallDLV
+		echo "" > $fileSummaryTime
+		for i in $dir/stats-*; do 
+			ctr=-1
+			while read LINE	
+			do
+				if [ $ctr -ge 0 ]; then
+					let data=$ctr%7
+					if [ $data -eq 2 ]; then
+						lastMI=$LINE
+					elif [ $data -eq 3 ]; then
+						lastSizeM=$LINE
+					elif [ $data -eq 4 ]; then
+						lastASDLV=$LINE
+					elif [ $data -eq 5 ]; then
+						lastCallDLV=$LINE
+					elif [ $data -eq 6 ]; then
+						lastTime=$LINE
+					fi
+				fi
+				let ctr=$ctr+1
+			done < $i
+			if [ $ctr -ge 6 ]; then
+				echo $lastMI >> $fileSummaryMI
+				echo $lastSizeM >> $fileSummarySizeM
+				echo $lastASDLV >> $fileSummaryASDLV
+				echo $lastCallDLV >> $fileSummaryCallDLV
+				echo $lastTime >> $fileSummaryTime
+			else
+				echo "$i"
+			fi
+		done
+	fi  
+done
