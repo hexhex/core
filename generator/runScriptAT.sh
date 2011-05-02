74aefaf6bd337dc06ba758044c843e04858126d5
Tri Kurniawan Wijaya <trikurniawanwijaya@gmail.com>
2011-05-02 13:34:50 +0000

    Change memory limit up to 2Gb.

--- generator/runScriptAT.sh
+++ generator/runScriptAT.sh
@@ -7,9 +7,10 @@
 #
 # run benchmark example for all topology in target directory
 #
-# require 2 params
-# 1st param: target directory, example: ModuleCore
-# 2nd param: result directory, example: StatsCore
+# require 3 params
+# 1st param: dlvhex+mlp command, for example: "dlvhex --mlp --verbose=128"
+# 2nd param: target directory, example: ModuleCore
+# 3rd param: result directory, example: StatsCore
 #
 
 targetDir=$2
@@ -18,8 +19,7 @@ resultDir=$3
 go=1
 if [ -a $resultDir ]; then
   go=0
-  echo "$resultDir is exist. Do you want to delete anyway? [y]es / [c]ancel execution: "
-  read inp
+  read -p "$resultDir is already exist. Do you want to delete it anyway? [y]es / [c]ancel execution: " inp
   if [ "$inp" = "y" ]; then
     rm -rf $resultDir
     go=1
@@ -43,7 +43,7 @@ for mainDir in $targetDir/*; do
 		echo "process $shortMainDir/$shortDir"
 		for i in {1..10}
 		do
-			(ulimit -v 1048576 ; /usr/bin/time --verbose -o $resultDir/$shortMainDir/$shortDir/time-$shortDir-i$i.log timelimit -p -s 1 -t 600 -T 5 $DLVHEX $mainDir/$shortDir/$shortDir-i$i-*.mlp) 2>$resultDir/$shortMainDir/$shortDir/stats-$shortDir-i$i.log 1>/dev/null
+			(ulimit -v 2097152 ; /usr/bin/time --verbose -o $resultDir/$shortMainDir/$shortDir/time-$shortDir-i$i.log timelimit -p -s 1 -t 300 -T 5 $DLVHEX $mainDir/$shortDir/$shortDir-i$i-*.mlp) 2>$resultDir/$shortMainDir/$shortDir/stats-$shortDir-i$i.log 1>/dev/null
 			echo "$i instances(s) processed"
 		done
 	fi
