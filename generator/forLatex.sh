2afe08d4ce1cc2b9d2643ffd225dfc4336bcc781
Tri Kurniawan Wijaya <trikurniawanwijaya@gmail.com>
2011-05-13 13:33:14 +0000

    Fix a small bug about opt.

--- generator/forLatex.sh
+++ generator/forLatex.sh
@@ -36,6 +36,7 @@ for mainDir in $targetDir/*; do #mainDir = Module-diamond, Module-line, etc
 	defRules=10
 	defModules=10
 	moduleName="${mainDir#$targetDir/}"
+	opt=""
 	if [ "$moduleName" = "Module-random" ];
 		then opt="-d$defDensity"
 	else 	if [ "$moduleName" = "Module-tree" ];
@@ -53,73 +54,73 @@ for mainDir in $targetDir/*; do #mainDir = Module-diamond, Module-line, etc
 		fi
 
 
-		echo "\hline" >> $mainDir/summaryProfLatex.txt		
 		for constant in {10,20,30,40} #looping constant
 		do 
 			string="$moduleName-$constant-$defPredicate-$head-$defBody-$not-$defRules-$defModules$opt"
 			analyze "$string" "$mainDir"
 		done
-
-
 		echo "\hline" >> $mainDir/summaryProfLatex.txt		
+
+
 		for predicate in {10,20,40} #looping constant
 		do 
 			string="$moduleName-$defConstant-$predicate-$head-$defBody-$not-$defRules-$defModules$opt"
 			analyze "$string" "$mainDir"
 		done
-
-
 		echo "\hline" >> $mainDir/summaryProfLatex.txt		
+
+
 		for modules in {5,10,15,20,25} #looping modules
 		do 
 			string="$moduleName-$defConstant-$defPredicate-$head-$defBody-$not-$defRules-$modules$opt"
 			analyze "$string" "$mainDir"
 		done
-
-
 		echo "\hline" >> $mainDir/summaryProfLatex.txt		
+
+
 		for body in {5,10,15,20} #looping body
 		do 
 			string="$moduleName-$defConstant-$defPredicate-$head-$body-$not-$defRules-$defModules$opt"
 			analyze "$string" "$mainDir"
 		done
-
-
 		echo "\hline" >> $mainDir/summaryProfLatex.txt		
+
+
 		for rules in {5,10,20,40} #looping rules
 		do 
 			string="$moduleName-$defConstant-$defPredicate-$head-$defBody-$not-$rules-$defModules$opt"
 			analyze "$string" "$mainDir"
 		done
+		echo "\hline" >> $mainDir/summaryProfLatex.txt		
+
 
 		if [ "$moduleName" = "Module-random" ];
 		then 
-			echo "\hline" >> $mainDir/summaryProfLatex.txt		
 			for density in {10,15,20,25} #looping density
 			do 
 				string="$moduleName-$defConstant-$defPredicate-$head-$defBody-$not-$defRules-$defModules-d$density"
 				analyze "$string" "$mainDir"
 			done
+			echo "\hline" >> $mainDir/summaryProfLatex.txt		
 				
 		else 	if [ "$moduleName" = "Module-tree" ];
 			then 
-				echo "\hline" >> $mainDir/summaryProfLatex.txt		
 				for branch in {2,3,5} #looping branch
 				do 
 					string="$moduleName-$defConstant-$defPredicate-$head-$defBody-$not-$defRules-$defModules-b$branch"
 					analyze "$string" "$mainDir"
 				done
+				echo "\hline" >> $mainDir/summaryProfLatex.txt		
 			fi
 		fi
 
 		#for big mlp
-		echo "\hline" >> $mainDir/summaryProfLatex.txt		
 		analyze "$moduleName-100-50-$head-$defBody-$not-10-50$opt" "$mainDir"
 		analyze "$moduleName-100-50-$head-$defBody-$not-20-50$opt" "$mainDir"
 		analyze "$moduleName-100-50-$head-$defBody-$not-10-100$opt" "$mainDir"
 		analyze "$moduleName-100-50-$head-$defBody-$not-20-100$opt" "$mainDir"
-
 		echo "\hline" >> $mainDir/summaryProfLatex.txt		
+
 	done
 
   fi
