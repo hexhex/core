1ad475ac1b98c50a9999311a434027988fea2ff5
Christoph Redl <redl@kr.tuwien.ac.at>
2012-11-19 20:39:19 +0100

    fix benchmark script

--- benchmarks/setminus/benchmark_all.sh
+++ benchmarks/setminus/benchmark_all.sh
@@ -14,7 +14,7 @@ do
 		output = $instance.out
 		error = $instance.error
 		Log = $instance.log
-		Requirements = machine == "node3.kr.tuwien.ac.at"
+		Requirements = machine == \"node3.kr.tuwien.ac.at\"
 		request_memory = 4096 
 		Initialdir = /mnt/lion/home/redl/hexhex_dlvhex/core/benchmarks/setminus/
 		notification = never
