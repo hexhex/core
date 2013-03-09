73f7354fc1e51c596b47efa896e284300585deca
Christoph Redl <Redl@credl-mac.local>
2013-03-09 20:06:46 +0100

    fix configure.ac

--- osxconfigure.sh
+++ osxconfigure.sh
@@ -1,2 +1,4 @@
 gsed -i.orig -e '/-Wl,-R/ { s/-Wl,-R/-Wl,-rpath,/; }' configure
+gsed -i.orig -e 's/-lclasp/${CLASP_TRUNK_DIR}\/build\/release\/libclasp\/*.o/' configure
+gsed -i.orig -e 's/-lappgringo -lgringo/${GRINGO_TRUNK_DIR}\/build\/release\/libprogram_opts\/CMakeFiles\/program_opts-lib.dir\/src\/*.o ${GRINGO_TRUNK_DIR}\/build\/release\/libgringo\/CMakeFiles\/gringo-lib.dir\/src\/*.o ${GRINGO_TRUNK_DIR}\/build\/release\/libgringo\/CMakeFiles\/gringo-lib.dir\/src\/output\/*.o/' configure
 bash configure $@
