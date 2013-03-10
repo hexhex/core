3120862ebaacc40439252c08105fb7f2013f9d79
Christoph Redl <redl@kr.tuwien.ac.at>
2013-03-10 15:15:25 +0100

    fix Makefile.am

--- build_potassco.sh
+++ build_potassco.sh
@@ -68,7 +68,7 @@ echo "making gringo"
 (
   cd gringo/build/release
   CXX=$CXX \
-  cmake ../../ -DCMAKE_CXX_FLAGS=-Wall -DCMAKE_BUILD_TYPE=release -DWITH_LUA=none ||
+  cmake ../../ -DCMAKE_CXX_FLAGS="-Wall -fPIC" -DCMAKE_BUILD_TYPE=release -DWITH_LUA=none ||
     { echo "gringo cmake failed!"; exit -1; }
   make gringo-app clingo-app VERBOSE=1 ||
     { echo "gringo make failed!"; exit -1; }
