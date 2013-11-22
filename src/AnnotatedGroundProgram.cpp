b3c76dfff19523320676d89e23715d748063bcde
Christoph Redl <redl@kr.tuwien.ac.at>
2013-11-22 23:52:06 +0100

    add missing dlvhex info to some cpp files

--- src/AnnotatedGroundProgram.cpp
+++ src/AnnotatedGroundProgram.cpp
@@ -1,3 +1,38 @@
+/* dlvhex -- Answer-Set Programming with external interfaces.
+ * Copyright (C) 2005, 2006, 2007 Roman Schindlauer
+ * Copyright (C) 2006, 2007, 2008, 2009, 2010 Thomas Krennwallner
+ * Copyright (C) 2009, 2010 Peter Schüller
+ * Copyright (C) 2011, 2012, 2013 Christoph Redl
+ * 
+ * This file is part of dlvhex.
+ *
+ * dlvhex is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * dlvhex is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with dlvhex; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ */
+
+
+/**
+ * @file   AnnotatedGroundProgram.cpp
+ * @author Christoph Redl
+ * @date Wed May 30 2012
+ * 
+ * @brief  Stores an ordinary ground program with some meta information,
+ * e.g. mapping of ground atoms back to external atoms, cycle information
+ * 
+ */
+
 #ifdef HAVE_CONFIG_H
 #include "config.h"
 #endif
--- src/ID.cpp
+++ src/ID.cpp
@@ -1,4 +1,34 @@
+/* dlvhex -- Answer-Set Programming with external interfaces.
+ * Copyright (C) 2005, 2006, 2007 Roman Schindlauer
+ * Copyright (C) 2006, 2007, 2008, 2009, 2010, 2011 Thomas Krennwallner
+ * Copyright (C) 2009, 2010, 2011 Peter Schüller
+ * Copyright (C) 2011, 2012, 2013 Christoph Redl
+ * 
+ * This file is part of dlvhex.
+ *
+ * dlvhex is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * dlvhex is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with dlvhex; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ */
 
+/**
+ * @file ID.cpp
+ * @author Peter Schueller
+ *
+ * @brief Implementation of the ID concept.
+ */
+ 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
 #endif // HAVE_CONFIG_H
