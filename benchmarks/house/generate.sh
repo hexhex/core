4bdcc6c527ffe268b62441bee3fed9cce8dd50f3
Christoph Redl <redl@kr.tuwien.ac.at>
2014-01-18 14:10:45 +0100

    update house benchmark

--- benchmarks/house/generate.sh
+++ benchmarks/house/generate.sh
@@ -1,24 +1,24 @@
 # $1: node count
 # $2: edge propability
 m=$(($1 * 3))
-echo "#maxint=$m."
+#echo "#maxint=$m."
 
 for (( i=1; i <= $1; i++ ))
 do
 	echo "person($i)."
 done
 
-for (( i=1; i <= 2 * $1; i++ ))
+for (( i=1; i <= $1 / 2 + $1 / 4 + $1; i++ ))
 do
         echo "cabinet($i)."
 done
 
-for (( i=1; i <= 1 + $1 / 2; i++ ))
+for (( i=1; i <= $1 / 2 + $1; i++ ))
 do
         echo "room($i)."
 done
 
-for (( i=1; i <= 3 * $1; i++ ))
+for (( i=1; i <= 2 * $1; i++ ))
 do
 	echo "thing($i)."
 	p=$((1 + $1 * $RANDOM / 32768))
