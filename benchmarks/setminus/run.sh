8366c069e7c2030884e718a76948ad684fc9e9b6
Christoph Redl <redl@kr.tuwien.ac.at>
2014-02-21 18:49:11 +0100

    add new benchmark script

---
+++ benchmarks/setminus/run.sh
@@ -0,0 +1,82 @@
+# check input validity
+inputok=1
+if [[ $# -eq 0 ]]; then
+	inputok=1
+elif [[ $# -gt 5 ]]; then
+	inputok=0
+elif [[ $1 != "all" ]] && [[ $1 != "single" ]]; then
+	inputok=0
+fi
+if [[ $inputok -eq 0 ]]; then
+	echo "This script expects 0, 1, 2, 4 or 5 parameters" >&2
+	echo "   \$1: (optional) \"all\" or \"single\", default is \"all\"" >&2
+	echo "   (a) If \$1=\"all\" then there are no further parameters" >&2
+	echo "       \$2: (optional) timeout, default is 300" >&2
+	echo "   (b) If \$1=\"single\" then" >&2
+	echo "       \$2: instance name" >&2
+	echo "       \$3: timeout in seconds" >&2
+	echo "       \$4: directory with the benchmark scripts" >&2
+	echo "       \$5: (optional) timeout, default is 300" >&2
+	exit 1
+fi
+
+# set default values
+if [[ $# -eq 0 ]]; then
+	all=1
+elif [[ $1 == "all" ]]; then
+	all=1
+else
+	all=0
+fi
+
+# get location of benchmark scripts
+if [[ $DLVHEX_BENCHMARKSCRIPTS != "" ]]; then
+	bmscripts=$DLVHEX_BENCHMARKSCRIPTS
+
+	if [[ $# == 2 ]]; then
+		to=$2
+	elif [[ $# == 5 ]]; then
+		to=$2
+	else
+		to=300
+	fi
+fi
+if [[ $all -eq 0 ]]; then
+	instance=$2
+	to=$3
+	bmscripts=$4
+fi
+if [[ $bmscripts == "" ]]; then
+	echo "Warning: Location of benchmark scripts not found; set variable DLVHEX_BENCHMARKSCRIPTS."
+fi
+
+# run instances
+mydir="$(dirname $0)"
+if [[ $all -eq 1 ]]; then
+	# run all instances using the benchmark script run insts
+	$bmscripts/runinsts.sh "{1..2}" "$mydir/run.sh" "$mydir" "$to"
+else
+	# run single instance
+	confstr="--flpcheck=explicit --noflpcriterion;--flpcheck=explicit --extlearn --noflpcriterion;--flpcheck=ufsm --noflpcriterion;--flpcheck=ufsm --extlearn --noflpcriterion;--flpcheck=ufsm --extlearn --ufslearn --noflpcriterion;--flpcheck=ufs;--flpcheck=ufs --extlearn;--flpcheck=ufs --extlearn --ufslearn;--flpcheck=aufs;--flpcheck=aufs --extlearn;--flpcheck=aufs --extlearn --ufslearn;--flpcheck=aufs --extlearn --ufslearn --ufscheckheuristics=periodic;--flpcheck=aufs --extlearn --ufslearn --ufscheckheuristics=max;--flpcheck=explicit --noflpcriterion -n=1;--flpcheck=explicit --extlearn --noflpcriterion -n=1;--flpcheck=ufsm -n=1;--flpcheck=ufsm --extlearn --noflpcriterion -n=1;--flpcheck=ufsm --extlearn --ufslearn --noflpcriterion -n=1;--flpcheck=ufs -n=1;--flpcheck=ufs --extlearn -n=1;--flpcheck=ufs --extlearn --ufslearn -n=1;--flpcheck=aufs -n=1;--flpcheck=aufs --extlearn -n=1;--flpcheck=aufs --extlearn --ufslearn -n=1;--flpcheck=aufs --extlearn --ufslearn --ufscheckheuristics=periodic -n=1;--flpcheck=aufs --extlearn --ufslearn --ufscheckheuristics=max -n=1"
+
+	# write instance file
+	inststr=`printf "%03d" ${instance}`
+	instfile=$(mktemp "inst_${inststr}_XXXXXXXXXX.hex")
+	if [[ $? -gt 0 ]]; then
+		echo "Error while creating temp file" >&2
+		exit 1
+	fi
+	prog="
+		nsel(X) :- domain(X), &testSetMinus[domain, sel](X)<monotonic domain,antimonotonic sel>.
+		sel(X) :- domain(X), &testSetMinus[domain, nsel](X)<monotonic domain, antimonotonic nsel>.
+		:- sel(X), sel(Y), sel(Z), X != Y, X != Z, Y != Z."
+	for (( j = 1; j <= $instance; j++ ))
+	do
+		prog="domain($j). $prog"
+	done
+	echo $prog > $instfile
+
+	$bmscripts/runconfigs.sh "dlvhex2 --plugindir=../../testsuite CONF INST" "$confstr" "$instfile" "$to"
+	rm $instfile
+fi
+
