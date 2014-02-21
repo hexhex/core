eb6049629e96c170a91ae606b3b3d9a41bef4924
Christoph Redl <redl@kr.tuwien.ac.at>
2014-02-21 22:44:25 +0100

    add benchmark script

---
+++ benchmarks/setminus-liberalsafety/run.sh
@@ -0,0 +1,93 @@
+# check input validity
+inputok=1
+if [[ $# -eq 0 ]]; then
+	inputok=1
+elif [[ $# -gt 5 ]]; then
+	inputok=0
+elif [[ $1 != "all" ]] && [[ $1 != "single" ]]; then
+	inputok=0
+fi
+if [[ $inputok -eq 0 ]]; then
+	echo "This script expects 0 to 5 parameters" >&2
+	echo "   \$1: (optional) \"all\" or \"single\", default is \"all\"" >&2
+	echo "   (a) If \$1=\"all\" then there are no further parameters" >&2
+	echo "       \$2: (optional) timeout, default is 300" >&2
+	echo "       \$3: (optional) directory with the benchmark scripts" >&2
+	echo "   (b) If \$1=\"single\" then" >&2
+	echo "       \$2: instance name" >&2
+	echo "       \$3: timeout in seconds" >&2
+	echo "       \$4: directory with the benchmark scripts" >&2
+	echo "       \$5: (optional) timeout, default is 300" >&2
+	exit 1
+fi
+
+# set default values
+# and get location of benchmark scripts
+if [[ $# -eq 0 ]]; then
+	all=1
+elif [[ $1 == "all" ]]; then
+	all=1
+else
+	all=0
+fi
+if [[ $all -eq 1 ]]; then
+	if [[ $# -ge 2 ]]; then
+		to=$2
+	else
+		to=300
+	fi
+	if [[ $# -ge 3 ]]; then
+		bmscripts=$3
+	else
+		runinstsdir=$(which runinsts.sh | head -n 1)
+		if [ -e "$runinstsdir" ]; then
+			bmscripts=$(dirname "$runinstsdir")
+		fi
+	fi
+else
+	instance=$2
+	to=$3
+	bmscripts=$4
+fi
+if ! [ -e "$bmscripts" ]; then
+	echo "Could not find benchmark scripts"
+	exit 1
+fi
+
+# run instances
+mydir="$(dirname $0)"
+if [[ $all -eq 1 ]]; then
+	# run all instances using the benchmark script run insts
+	$bmscripts/runinsts.sh "{1..20}" "$mydir/run.sh" "$mydir" "$to"
+else
+	# run single instance
+	confstr="--extlearn --flpcheck=aufs prog$instance.hex;--extlearn --flpcheck=aufs --liberalsafety prognd$instance.hex"
+
+	inststr=`printf "%03d" ${instance}`
+
+	# write instance files
+	prog="
+		nsel(X) :- domain(X), &testSetMinus[domain, sel](X)<monotonic domain,antimonotonic sel>.
+		sel(X) :- domain(X), &testSetMinus[domain, nsel](X)<monotonic domain, antimonotonic nsel>.
+		:- sel(X), sel(Y), sel(Z), X != Y, X != Z, Y != Z."
+	for (( j = 1; j <= $instance; j++ ))
+	do
+		prog="domain($j). $prog"
+	done
+	echo $prog > prog$instance.hex
+
+	prog="
+		nsel(X) :- &testSetMinus[domain, sel](X)<monotonic domain,antimonotonic sel,relativefinitedomain 0 domain>.
+		sel(X) :- &testSetMinus[domain, nsel](X)<monotonic domain, antimonotonic nsel,relativefinitedomain 0 domain>.
+		:- sel(X), sel(Y), sel(Z), X != Y, X != Z, Y != Z."
+	for (( j = 1; j <= $instance; j++ ))
+	do
+		prog="domain($j). $prog"
+	done
+	echo $prog > prognd$instance.hex
+
+	$bmscripts/runconfigs.sh "dlvhex2 --plugindir=../../testsuite CONF" "$confstr" "$inststr" "$to" "$bmscripts/gstimeoutputbuilder.sh"
+	rm prog$instance.hex
+	rm prognd$instance.hex
+fi
+
