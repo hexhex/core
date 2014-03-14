037a7e4d5c9dc7eec23c7eb74a10eb46a1641c89
Christoph Redl <redl@kr.tuwien.ac.at>
2014-03-14 14:22:36 +0100

    upgrade to Clasp 3.0.1

--- src/GringoGrounder.cpp
+++ src/GringoGrounder.cpp
@@ -26,7 +26,7 @@
  * @file ClaspSolver.cpp
  * @author Christoph Redl
  *
- * @brief Interface to genuine gringo 3.0.4-based grounder.
+ * @brief Interface to genuine gringo 4.3.0-based grounder.
  */
 
 #ifdef HAVE_CONFIG_H
