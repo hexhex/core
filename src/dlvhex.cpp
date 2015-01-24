f54d541c52c9e2c1c0365d4559e444c11a07710c
Christoph Redl <redl@kr.tuwien.ac.at>
2015-01-25 00:40:53 +0100

    enable liberal safety by default

--- src/dlvhex.cpp
+++ src/dlvhex.cpp
@@ -450,7 +450,7 @@ int main(int argc, char *argv[])
   pctx.config.setOption("Forget", 0);
   pctx.config.setOption("Split", 0);
   pctx.config.setOption("SkipStrongSafetyCheck",0);
-  pctx.config.setOption("LiberalSafety",0);
+  pctx.config.setOption("LiberalSafety",1);
   pctx.config.setOption("LiberalSafetyNullFreezeCount",0);	// necessary for existential quantification, see ExistsPlugin.cpp
   pctx.config.setOption("LiberalSafetyHomomorphismCheck",0);	// necessary for existential quantification, see ExistsPlugin.cpp
   pctx.config.setOption("WellJustified",0);
