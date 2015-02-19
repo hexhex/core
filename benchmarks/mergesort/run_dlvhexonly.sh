55de71d63e688478db45dd738b6816e4715cecd9
Christoph Redl <redl@kr.tuwien.ac.at>
2015-02-19 15:13:36 +0100

    extend mergesort to include dlv

---
+++ benchmarks/mergesort/run_dlvhexonly.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+runheader=$(which dlvhex_run_header.sh)
+if [[ $runheader == "" ]] || [ $(cat $runheader | grep "dlvhex_run_header.sh Version 1." | wc -l) == 0 ]; then
+	echo "Could not find dlvhex_run_header.sh (version 1.x); make sure that the benchmarks/script directory is in your PATH"
+	exit 1
+fi
+source $runheader
+
+# run instances
+if [[ $all -eq 1 ]]; then
+	# run all instances using the benchmark script run insts
+	$bmscripts/runinsts.sh "instances/*.hex" "$mydir/run_dlvhexonly.sh" "$mydir" "$to" "" "" "$req"
+else
+	# run single instance
+	confstr="dlvhex2 --plugindir=../../testsuite --verbose=8 --extlearn --flpcheck=aufs --ufslearn=none --liberalsafety mergesort.hex -n=1 $instance;dlvhex2 --plugindir=../../testsuite --verbose=8 --extlearn --flpcheck=aufs --ufslearn=none --strongsafety mergesort_strongsafety.hex -n=1 $instance"
+
+	$bmscripts/runconfigs.sh "CONF" "$confstr" "$instance" "$to" "$bmscripts/gstimeoutputbuilder.sh"
+fi
+
