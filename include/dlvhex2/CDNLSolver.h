f33f716339167af0a14bc98fa87c8417c7aa9a54
Christoph Redl <redl@kr.tuwien.ac.at>
2015-03-02 14:30:22 +0100

    extend documentation

--- include/dlvhex2/CDNLSolver.h
+++ include/dlvhex2/CDNLSolver.h
@@ -54,11 +54,13 @@ class PropagatorCallback;
 
 class CDNLSolver : virtual public NogoodContainer, virtual public SATSolver{
 protected:
+	/** \brief Allows for hashing IDAddress. */
 	struct SimpleHashIDAddress : public std::unary_function<IDAddress, std::size_t> {
 		inline std::size_t operator()(IDAddress const& ida) const{
 			return ida;
 		}
 	};
+	/** \brief Allows for hashing ID. */
 	struct SimpleHashID : public std::unary_function<ID, std::size_t> {
 		inline std::size_t operator()(ID const& id) const{
 			if (id.isNaf())	return id.address * 2 + 1;
@@ -67,53 +69,89 @@ protected:
 	};
 
 	// instance information
+	/** \brief Instance. */
 	NogoodSet nogoodset;
+	/** \brief Facts of the instance as Set. */
 	Set<IDAddress> allFacts;
+	/** \brief ProgramCtx. */
 	ProgramCtx& ctx;
+	/** \brief Nogoods scheduled for adding but not added yet. */
 	SimpleNogoodContainer nogoodsToAdd;
 
 	// solver state information
+	/** \brief Current (partial) interpretation. */
 	InterpretationPtr interpretation;
+	/** \brief Set of assigned atoms. */
 	InterpretationPtr factWasSet;
+	/** \brief Decision level for each atom; undefined if unassigned. */
 	DynamicVector<IDAddress, int> decisionlevel;
+	/** \brief Stores for each atom the index and hash of the clause which implied it; undefined if unassigned or guessed. */
 	boost::unordered_map<IDAddress, int, SimpleHashIDAddress> cause;
+	/** \brief Current decision level >= 0. */
 	int currentDL;
+	/** \brief Stores the current assignment in chronoligical order. */
 	OrderedSet<IDAddress, SimpleHashIDAddress> assignmentOrder;
+	/** \brief Stores for each decision level the facts assigned on that level. */
 	DynamicVector<int, std::vector<IDAddress> > factsOnDecisionLevel;
 
-	int exhaustedDL;	// maximum decision level such that the search space above was exhausted
+	/** \brief Maximum decision level such that the search space above was exhausted
+	  * (used for eliminating duplicate models without explicitly adding them as nogoods). */
+	int exhaustedDL;
+	/** \brief Stores for each decision level the guessed literal (=decision literal). */
 	DynamicVector<int, ID> decisionLiteralOfDecisionLevel;
 
 	// watching data structures for efficient unit propagation
+	/** \brief Stores for each positive literal the nogoods which contain. */
 	boost::unordered_map<IDAddress, Set<int>, SimpleHashIDAddress > nogoodsOfPosLiteral;
+	/** \brief Stores for each negative literal the nogoods which contain. */
 	boost::unordered_map<IDAddress, Set<int>, SimpleHashIDAddress > nogoodsOfNegLiteral;
+	/** \brief Stores for each positive literal the nogoods which watch it (i.e., which might fire once the literal becomes true). */
 	boost::unordered_map<IDAddress, Set<int>, SimpleHashIDAddress > watchingNogoodsOfPosLiteral;
+	/** \brief Stores for each negative literal the nogoods which watch it (i.e., which might fire once the literal becomes true). */
 	boost::unordered_map<IDAddress, Set<int>, SimpleHashIDAddress > watchingNogoodsOfNegLiteral;
+	/** \brief Stores for each nogood the set of watched literals. */
 	std::vector<Set<ID> > watchedLiteralsOfNogood;
+	/** \brief Stores the nogoods which are currently unit (i.e., all literals but one are satisfied). */
 	Set<int> unitNogoods;
+	/** \brief Stores the nogoods which are currently contradictory (i.e., all literals are satisfied). */
 	Set<int> contradictoryNogoods;
 
 	// variable selection heuristics
+	/** \brief Counter for total number of conflicts so far (periodic reset). */
 	int conflicts;
+	/** \brief Counts for each positive literal the number of conflicts it was involved in (periodic reset). */
 	boost::unordered_map<IDAddress, int, SimpleHashIDAddress> varCounterPos;
+	/** \brief Counts for each negative literal the number of conflicts it was involved in (periodic reset). */
 	boost::unordered_map<IDAddress, int, SimpleHashIDAddress> varCounterNeg;
+	/** \brief Stores the indexes of the clauses which were recently contradictory in chronological order. */
 	std::vector<int> recentConflicts;
 
 	// statistics
+	/** \brief Number of assignments so far. */
 	long cntAssignments;
+	/** \brief Number of guesses so far. */
 	long cntGuesses;
+	/** \brief Number of backtracks so far. */
 	long cntBacktracks;
+	/** \brief Number of resolution steps so far. */
 	long cntResSteps;
+	/** \brief Number of conflicts so far. */
 	long cntDetectedConflicts;
 
-	// temporary objects (they are just class members in order to make them reuseable without reallocation)
+	/** \brief Temporary objects (they are just class members in order to make them reuseable without reallocation). */
 	Set<ID> tmpWatched;
 
 	// members
+	/** \brief Checks if an atom is assigned.
+	  * @param litadr Atom IDAddress.
+	  * @return True if \p litadr is assigned and false otherwise. */
 	inline bool assigned(IDAddress litadr){
 		return factWasSet->getFact(litadr);
 	}
 
+	/** \brief Checks if a literal is satisfied.
+	  * @param lit Literal ID.
+	  * @return True if \p lit is satisfied and false otherwise. */
 	inline bool satisfied(ID lit){
 		// fact must have been set
 		if (!assigned(lit.address)) return false;
@@ -121,6 +159,9 @@ protected:
 		return interpretation->getFact(lit.address) == !lit.isNaf();
 	}
 
+	/** \brief Checks if a literal is falsified.
+	  * @param litlitadr Literal ID.
+	  * @return True if \p lit is falsified and false otherwise. */
 	inline bool falsified(ID lit){
 		// fact must have been set
 		if (!assigned(lit.address)) return false;
@@ -128,10 +169,15 @@ protected:
 		return interpretation->getFact(lit.address) != !lit.isNaf();
 	}
 
+	/** \brief Negates a literal ID.
+	  * @param lit Literal ID.
+	  * @return Negated literal ID. */
 	inline ID negation(ID lit){
 		return ID(lit.kind ^ ID::NAF_MASK, lit.address);
 	}
 
+	/** \brief Checks if the assignment is currently complete.
+	  * @return True if complete and false otherwise. */
 	inline bool complete(){
 		return factWasSet->getStorage().count() == allFacts.size();
 	}
@@ -150,32 +196,66 @@ protected:
 	void flipDecisionLiteral();
 
 	// members for maintaining the watching data structures
+	/** \brief Performs unit propagation. */
 	void initWatchingStructures();
+	/** \brief Updates all data structures after a nogood was added.
+	  * @param index Index of added nogood. */
 	void updateWatchingStructuresAfterAddNogood(int index);
+	/** \brief Updates all data structures after a nogood was removed
+	  * @param index Former index of removed nogood. */
 	void updateWatchingStructuresAfterRemoveNogood(int index);
+	/** \brief Updates all data structures after a literal was assigned to true.
+	  * @param lit Literal which is now true. */
 	void updateWatchingStructuresAfterSetFact(ID lit);
+	/** \brief Updates all data structures after a literal was unassigned.
+	  * @param lit Literal which is now unassigned. */
 	void updateWatchingStructuresAfterClearFact(ID lit);
+	/** \brief Removed all watches for a nogood.
+	  * @param nogoodNr Index of the nogood to remove all watches for. */
 	void inactivateNogood(int nogoodNr);
+	/** \brief Removed a single watche for a nogood.
+	  * @param nogoodNr Index of the nogood to remove the watche for.
+	  * @param lit The literal which shall not be watched any longer. */
 	void stopWatching(int nogoodNr, ID lit);
+	/** \brief Adds a single watche for a nogood.
+	  * @param nogoodNr Index of the nogood to add the watche for.
+	  * @param lit The literal which shall now be watched. */
 	void startWatching(int nogoodNr, ID lit);
 
 	// members for variable selection heuristics
+	/** \brief Increses the usage counter for all variables in a nogood.
+	  * @param ng The nogood whose variables shall be touched. */
 	void touchVarsInNogood(Nogood& ng);
 
 	// external learning
+	/** \brief Set of atoms which (possibly) changes since last call of external learners because they have been reassigned. */
 	InterpretationPtr changed;
+	/** \brief Set of external propagators. */
 	Set<PropagatorCallback*> propagator;
 
 	// initialization members
+	/** \brief Harvests all atoms in the instance. */
 	void initListOfAllFacts();
+	/** \brief Resizes the internal solver vectors according to the total number of ground atoms in the registry. */
 	virtual void resizeVectors();
 
 	// helper members
+	/** \brief Encodes a literal in a string.
+	  * @param lit Literal to encode.
+	  * @return String representation of \p lit. */
 	static std::string litToString(ID lit);
+	/** \brief Checks if a vector contains an element.
+	  * @param s Vector.
+	  * @param el Element.
+	  * @return True if \p el is in \p s and false otherwise. */
 	template <typename T>
 	inline bool contains(const std::vector<T>& s, T el){
 		return std::find(s.begin(), s.end(), el) != s.end();
 	}
+	/** \brief Checks if two vectors intersect.
+	  * @param a First vector.
+	  * @param b Second vector.
+	  * @return True if \p a and \p b intersect and false otherwise. */
 	template <typename T>
 	inline std::vector<T> intersect(const std::vector<T>& a, const std::vector<T>& b){
 		std::vector<T> i;
@@ -185,14 +265,25 @@ protected:
 		return i;
 	}
 
+	/** \brief Retrieves for an atom the index in the assignment list in chronological order.
+	  * @param adr Atom IDAddress.
+	  * @return -1 if \p adr is unassigned and its assignment index otherwise (larger means more recent). */
 	long getAssignmentOrderIndex(IDAddress adr){
 		if (!assigned(adr)) return -1;
 		return assignmentOrder.getInsertionIndex(adr);
 	}
 
+	/** \brief Adds a nogood and updates all internal data structures.
+	  * @param ng Nogood to add.
+	  * @return Index of the new nogood. */
 	int addNogoodAndUpdateWatchingStructures(Nogood ng);
 
+	/** \brief Retrieves all nogoods which are currently conflicting.
+	  * @return Set of conflicting nogoods. */
 	std::vector<Nogood> getContradictoryNogoods();
+	/** \brief Checks if a given atom is used as decision literal.
+	  * @param litaddr Atom address.
+	  * @return True if \p litaddr is a decision literal and false otherwise. */
 	inline bool isDecisionLiteral(IDAddress litaddr){
 		if (cause[litaddr] == -1){
 			return true;
@@ -200,8 +291,18 @@ protected:
 			return false;
 		}
 	}
+	/** \brief Retrieves for an atom the nogood which implied it.
+	  * @param adr Address of an assigned and not guessed atom.
+	  * @return Nogood which implied \p adr. */
 	Nogood getCause(IDAddress adr);
 public:
+	/**
+	  * \brief Constructor.
+	  *
+	  * Initializes the solver.
+	  * @param ctx ProgramCtx.
+	  * @param ns Instance as NogoodSet.
+	  */
 	CDNLSolver(ProgramCtx& ctx, NogoodSet ns);
 	virtual void addNogoodSet(const NogoodSet& ns, InterpretationConstPtr frozen = InterpretationConstPtr());
 
@@ -210,6 +311,13 @@ public:
 	virtual void removePropagator(PropagatorCallback* pb);
 	virtual InterpretationPtr getNextModel();
 	virtual void addNogood(Nogood ng);
+
+	/**
+	  * \brief Delivers solver statistics in human-readable format.
+	  *
+	  * \brief Create solver statistics in a no further specified format (for debug purposes).
+	  * @return Debug statistics.
+	  */
 	virtual std::string getStatistics();
 
 	typedef boost::shared_ptr<CDNLSolver> Ptr;
--- include/dlvhex2/InternalGroundASPSolver.h
+++ include/dlvhex2/InternalGroundASPSolver.h
@@ -56,83 +56,184 @@ DLVHEX_NAMESPACE_BEGIN
 	// (there is a compiler bug in MSVC; the call of addNogood is actually _not_ ambigious because the method is pure virtual in GenuineGroundSolver)
 	#pragma warning (disable: 4250)
 #endif
+/** \brief Implements an internal ASP solver without using third-party software. */
 class InternalGroundASPSolver : public CDNLSolver, public GenuineGroundSolver{
 private:
+	/** \brief Prefix added to variables introduced to represent rule bodies. */
 	std::string bodyAtomPrefix;
+	/** \brief Counter for variables introduced for rule bodies so far. */
 	int bodyAtomNumber;
 
 protected:
+	/** \brief True before the first model was found and false otherwise. */
 	bool firstmodel;
+	/** \brief Number of models found so far. */
 	int modelCount;
 
 protected:
 	// structural program information
+	/** \brief Problem instance, i.e., ASP program. */
 	AnnotatedGroundProgram program;
+	/** \brief RegistryPtr. */
 	RegistryPtr reg;
 
+	/** \brief Set of facts in the program as Set. */
 	Set<IDAddress> ordinaryFacts;
+	/** \brief Set of facts in the program as Interpretation. */
 	InterpretationPtr ordinaryFactsInt;
+	/** \brief Set of facts which occur in non-singular strongly connected components in the positive atom dependency graph. */
 	Set<IDAddress> nonSingularFacts;
 
-										// dependency graph
+	// dependency graph
 	typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::bidirectionalS, IDAddress> Graph;
 	typedef Graph::vertex_descriptor Node;
+	/** \brief Nodes in the positive atom dependency graph. */
 	boost::unordered_map<IDAddress, Node, SimpleHashIDAddress> depNodes;
+	/** \brief Positive atom dependency graph. */
 	Graph depGraph;
 
-	std::vector<Set<IDAddress> > depSCC;					// store for each component the contained atoms
-	boost::unordered_map<IDAddress, int, SimpleHashIDAddress> componentOfAtom;// store for each atom its component number
-	boost::unordered_map<IDAddress, IDAddress, SimpleHashIDAddress> bodyAtomOfRule;	// store for each rule the body atom
+	/** \brief Stores for each component the contained atoms. */
+	std::vector<Set<IDAddress> > depSCC;
+	/** \brief Stores for each atom its component number. */
+	boost::unordered_map<IDAddress, int, SimpleHashIDAddress> componentOfAtom;
+	/** \brief Store for each rule the body atom. */
+	boost::unordered_map<IDAddress, IDAddress, SimpleHashIDAddress> bodyAtomOfRule;
 
 
 	// data structures for unfounded set computation
-	Set<IDAddress> unfoundedAtoms;						// currently unfounded atoms
-	boost::unordered_map<IDAddress, Set<ID>, SimpleHashIDAddress > rulesWithPosBodyLiteral;	// store for each literal the rules which contain it positively in their body
-	boost::unordered_map<IDAddress, Set<ID>, SimpleHashIDAddress > rulesWithNegBodyLiteral;	// store for each literal the rules which contain it negatively in their body
-	boost::unordered_map<IDAddress, Set<ID>, SimpleHashIDAddress > rulesWithPosHeadLiteral;	// store for each literal the rules which contain it (positively) in their head
-	boost::unordered_map<IDAddress, Set<IDAddress>, SimpleHashIDAddress > foundedAtomsOfBodyAtom;// store for each body atom the set of atoms which use the corresponding rule as source
-	boost::unordered_map<IDAddress, ID, SimpleHashIDAddress> sourceRule;	// store for each atom a source rule (if available); for facts, ID_FAIL will be stored
+	/** \brief Currently unfounded atoms. */
+	Set<IDAddress> unfoundedAtoms;
+	/** \brief Stores for each literal the rules which contain it positively in their body. */
+	boost::unordered_map<IDAddress, Set<ID>, SimpleHashIDAddress > rulesWithPosBodyLiteral;
+	/** \brief Stores for each literal the rules which contain it negatively in their body. */
+	boost::unordered_map<IDAddress, Set<ID>, SimpleHashIDAddress > rulesWithNegBodyLiteral;
+	/** \brief Stores for each literal the rules which contain it (positively) in their head. */
+	boost::unordered_map<IDAddress, Set<ID>, SimpleHashIDAddress > rulesWithPosHeadLiteral;
+	/** \brief Stores for each body atom the set of atoms which use the corresponding rule as source. */
+	boost::unordered_map<IDAddress, Set<IDAddress>, SimpleHashIDAddress > foundedAtomsOfBodyAtom;
+	/** \brief Stores for each atom a source rule (if available); for facts, ID_FAIL will be stored. */
+	boost::unordered_map<IDAddress, ID, SimpleHashIDAddress> sourceRule;
 
 	// statistics
+	/** \brief Number of unfounded sets detected so far. */
 	long cntDetectedUnfoundedSets;
 
 	// initialization members
+	/** \brief Adds nogoods for a rule.
+	  * @param ruleBodyAtomID Atom used for representing the rule body.
+	  * @param ruleID ID of the rule to encode. */
 	void createNogoodsForRule(ID ruleBodyAtomID, ID ruleID);
+	/** \brief Adds nogoods for a rule.
+	  * @param ruleBodyAtomID Atom used for representing the rule body.
+	  * @param ruleBody Rule body to encode. */
 	void createNogoodsForRuleBody(ID ruleBodyAtomID, const Tuple& ruleBody);
+	/** \brief Creates the shifted program.
+	  * 
+	  * For each rule of kind h1 v ... hn :- b1, ..., bm the shifted program
+	  * contains all shifted rules of kind hi :- b1, ..., bm, not h1, ..., not h{i-1}, not h{i+1}, ..., not hn.
+	  * @return For each rule the set of shifted rules. */
 	Set<std::pair<ID, ID> > createShiftedProgram();
+	/** \brief Computes Clark's completion of the input program and adds it to the internal instance. */
 	void computeClarkCompletion();
+	/** \brief Computes loop nogoods for singular components of the input program and adds it to the internal instance. */
 	void createSingularLoopNogoods();
 	virtual void resizeVectors();
+	/** \brief Assigns all atoms from the EDB in the interpretation. */
 	void setEDB();
+	/** \brief Computes the positive atom dependency graph of the input program. */
 	void computeDepGraph();
+	/** \brief Computes the strongly connected components of the positive atom dependency graph of the input program. */
 	void computeStronglyConnectedComponents();
+	/** \brief Initializes the source pointer data structures for unfounded set detection. */
 	void initSourcePointers();
+	/** \brief Initializes all lists of atoms and facts. */
 	void initializeLists();
 
 	// unfounded set members
 	virtual void setFact(ID fact, int dl, int cause);
 	virtual void clearFact(IDAddress litadr);
+	/** \brief Removes a source pointer from an atom.
+	  * @param litadr Atom to remove the source pointer from. */
 	void removeSourceFromAtom(IDAddress litadr);
-	void addSourceToAtom(IDAddress, ID rule);
+	/** \brief Adds a rule as a possible source for deriving an atom.
+	  * @param litadr Atom IDAddress.
+	  * @param rule ID of a rule which may derive \p litadr. */
+	void addSourceToAtom(IDAddress litadr, ID rule);
+	/** \brief Retrieves a list of all atoms which might transitively depend on \p litadr.
+	  * @param litadr IDAddress of an atom.
+	  * @return List of all atoms which might transitively depend on \p litadr. */
 	Set<IDAddress> getDependingAtoms(IDAddress litadr);
+	/** \brief Get the set of atoms which become unfounded after a literal was assigned.
+	  * @param fact Literal which is now true (either a positive or a negated atom).
+	  * @param newlyUnfoundedAtoms Set of atoms which are unfounded after \p fact was assigned. */
 	void getInitialNewlyUnfoundedAtomsAfterSetFact(ID fact, Set<IDAddress>& newlyUnfoundedAtoms);
+	/** \brief Bookkeeping for internal data structures after a literal became true.
+	  * @param fact Literal which is now true. */
 	void updateUnfoundedSetStructuresAfterSetFact(ID fact);
+	/** \brief Bookkeeping for internal data structures after a literal became unassigned.
+	  * @param fact Literal which is now unassigned. */
 	void updateUnfoundedSetStructuresAfterClearFact(IDAddress litadr);
 	ID getPossibleSourceRule(const Set<ID>& ufs);
+	/** \brief Checks if a head atom uses the rule as source.
+	   * @param headAtom Atom a an atom in the head of \p sourceRule.
+	   * @param sourceRule ID of a rule.
+	   * @return True if 1. the \p headAtom is currently unfounded and 2. no other head literal of rule \p sourceRule was set to true earlier. */
 	bool useAsNewSourceForHeadAtom(IDAddress headAtom, ID sourceRule);
+	/** \brief Finds an unfounded set.
+	  * @return A non-empty unfounded set if there is any, and an empty set otherwise. */
 	Set<ID> getUnfoundedSet();
 
 	// helper members
+	/** \brief Checks for a rule if it supports a literal externally to a set \p s.
+	  *
+	  * External support means that the rule may be used to derive the atom
+	  * but does not depend on an atom in \p s.
+	  *
+	  * @param ruleID ID of a rule.
+	  * @param lit Literal ID.
+	  * @param s Set of literals.
+	  * @return True if the rule supports \p lit externally wrt. \p s and false otherwise. */
 	bool doesRuleExternallySupportLiteral(ID ruleID, ID lit, const Set<ID>& s);
+	/** \brief Finds all rules which support some atom from \p s externally wrt. \p s.
+	  * @param s Set of literals.
+	  * @return Set of all rules which support some atom from \p s externally wrt. \p s. */
 	Set<ID> getExternalSupport(const Set<ID>& s);
+	/** \brief Compute all literals which satisfy the rule independently of set \p y.
+	  *
+	  * This is the case if either the body of rule \p ruleID is false or
+	  * some head literal, which is not in \p y, is true.
+	  * @param ruleID ID of some rule.
+	  * @param y Some set of literals.
+	  * @return All literals which satisfy the rule independently of set \p y. */
 	Set<ID> satisfiesIndependently(ID ruleID, const Set<ID>& y);
+	/** \brief Constructs a loop nogood for an unfouneded set.
+	  * @param ufs Unfounded set.
+	  * @param Nogood which tries to avoid the same unfounded set in the future search. */
 	Nogood getLoopNogood(const Set<ID>& ufs);
+	/** \brief Adds a new propositional atom.
+	  * @param predID Predicate used for the new atom.
+	  * @return ID of the new atom. */
 	ID createNewAtom(ID predID);
+	/** \brief Adds a new atom used for representing a rule body.
+	  * @return ID of the new atom. */
 	ID createNewBodyAtom();
+	/** \brief Returns a string representation of a set of literals.
+	  * @param lits Set of literals.
+	  * @return String representation of \p lits. */
 	std::string toString(const Set<ID>& lits);
+	/** \brief Returns a string representation of a set of atoms.
+	  * @param lits Set of atoms.
+	  * @return String representation of \p lits. */
 	std::string toString(const Set<IDAddress>& lits);
+	/** \brief Returns a string representation of a vector of atoms.
+	  * @param lits Vector of atoms.
+	  * @return String representation of \p lits. */
 	std::string toString(const std::vector<IDAddress>& lits);
 
+	/** \brief Intersects two sets.
+	  * @param a First Set.
+	  * @param b Second Set.
+	  * @return Intersection of \p a and \p b as std::vector. */
 	template <typename T>
 	inline std::vector<T> intersect(const Set<T>& a, const std::vector<T>& b){
 		std::vector<T> i;
@@ -141,6 +242,11 @@ protected:
 		}
 		return i;
 	}
+
+	/** \brief Intersects two sets.
+	  * @param a First Set.
+	  * @param b Second Set.
+	  * @return Intersection of \p a and \p b as Set. */
 	template <typename T>
 	inline Set<T> intersect(const Set<T>& a, const Set<T>& b){
 		Set<T> out;
@@ -148,8 +254,18 @@ protected:
 		return out;
 	}
 
-	InterpretationPtr outputProjection(InterpretationConstPtr intr);	// projects dummy atoms for rule bodies away
+	/** \brief Projects dummy atoms for rule bodies away.
+	  * @param intr Interpretation to project.
+	  * @return Projection of \p intr. */
+	InterpretationPtr outputProjection(InterpretationConstPtr intr);
 public:
+	/**
+	  * \brief Constructor.
+	  *
+	  * Initializes the solver.
+	  * @param ctx ProgramCtx.
+	  * @param ns Instance as NogoodSet.
+	  */
 	InternalGroundASPSolver(ProgramCtx& ctx, const AnnotatedGroundProgram& p);
 	virtual void addProgram(const AnnotatedGroundProgram& p, InterpretationConstPtr frozen = InterpretationConstPtr());
 	virtual void addNogoodSet(const NogoodSet& ns, InterpretationConstPtr frozen = InterpretationConstPtr());
