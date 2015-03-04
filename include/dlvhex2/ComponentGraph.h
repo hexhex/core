b1d443fc97b16c73de28de0cf02fd4870c911456
Christoph Redl <redl@kr.tuwien.ac.at>
2015-03-04 17:30:04 +0100

    extend documentation

--- include/dlvhex2/ComponentGraph.h
+++ include/dlvhex2/ComponentGraph.h
@@ -44,7 +44,8 @@
 
 DLVHEX_NAMESPACE_BEGIN
 
-/**
+/** \brief Implements the component graph.
+ *
  * A component graph is created from a dependency graph by collecting SCCs
  * into single nodes components.
  *
@@ -72,37 +73,50 @@ class DLVHEX_EXPORT ComponentGraph
   // types
   //////////////////////////////////////////////////////////////////////////////
 public:
+  /** \brief Implements meta information about components. */
   struct DLVHEX_EXPORT ComponentInfo:
     public ostream_printable<ComponentInfo>
   {
     #ifdef COMPGRAPH_SOURCESDEBUG
+    /** \brief List of nodes. */
     std::list<DependencyGraph::Node> sources;
     #endif
 
+    /** \brief Outer external atoms in the component, i.e., external atoms which do not depend on atoms defined in this component. */
     std::vector<ID> outerEatoms;
 
+    /** \brief Inner rules in the component (except constraints), i.e., rules which may cyclically depend on atoms defined in this component. */
     std::vector<ID> innerRules;
+    /** \brief Inner external atoms in the component, i.e., external atoms which cyclically depend on atoms defined in this component. */
     std::vector<ID> innerEatoms;
+    /** \brief Inner constraints in the component, i.e., rules which may cyclically depend on atoms defined in this component. */
     std::vector<ID> innerConstraints;
+    /** \brief Stores for each rule the set of strongly safe variables in the rule. */
     boost::unordered_map<ID, std::set<ID> > stronglySafeVariables;
+    /** \brief Stores for each rule the set of stratified literals in the rule, i.e., which do not depend on atoms derived in the component. */
     boost::unordered_map<ID, std::set<ID> > stratifiedLiterals;
+    /** \brief Set of all predicates defined in the component. */
     std::set<ID> predicatesInComponent;
 
     // this is determined by calculateComponents
     // and used for selecting model generator factories
-		bool disjunctiveHeads;
-		bool negativeDependencyBetweenRules;
-		bool innerEatomsNonmonotonic;
-		bool outerEatomsNonmonotonic;
-		bool componentIsMonotonic;
-		bool fixedDomain;
-		bool recursiveAggregates;
+    /** \brief Component contains disjunctive heads. */
+    bool disjunctiveHeads;
+    /** \brief Component contains negative dependencies between rules. */
+    bool negativeDependencyBetweenRules;
+    /** \brief Component contains nonmonotonic inner external atoms. */
+    bool innerEatomsNonmonotonic;
+    /** \brief Component contains nonmonotonic outer external atoms. */
+    bool outerEatomsNonmonotonic;
+    /** \brief Component is purely monotonic. */
+    bool componentIsMonotonic;
+    /** \brief Component does not make use of value invention. */
+    bool fixedDomain;
+    /** \brief Component contains recursive aggregates. */
+    bool recursiveAggregates;
 
-		// previous:
-    //bool innerEatomsMonotonicAndOnlyPositiveCycles;
-		// := (!innerEatomsNonmonotonic && !negativeDependencyBetweenRules && !disjunctiveHeads)
-
-		ComponentInfo():
+    /** \brief Constructor. */
+    ComponentInfo():
       disjunctiveHeads(false),
 			negativeDependencyBetweenRules(false),
 			innerEatomsNonmonotonic(false),
@@ -113,19 +127,25 @@ public:
     std::ostream& print(std::ostream& o) const;
   };
 
+  /** \brief Meta information about rule dependencies in the component. */
   struct DependencyInfo:
 		public DependencyGraph::DependencyInfo,
     public ostream_printable<DependencyInfo>
   {
     #ifdef COMPGRAPH_SOURCESDEBUG
+    /** \brief Dependencies in the underlying graph. */
     std::set<DependencyGraph::Dependency> sources;
     #endif
 
     typedef boost::tuple<ID, ID, DependencyGraph::DependencyInfo> DepEdge;
+    /** \brief Set of edges in the component. */
     std::vector<DepEdge> depEdges;
 
-		DependencyInfo() {}
-		DependencyInfo(const DependencyGraph::DependencyInfo& other):
+    /** \brief Constructor. */
+    DependencyInfo() {}
+    /** \brief Copy-constructor.
+      * @param other DependencyInfo to copy. */
+    DependencyInfo(const DependencyGraph::DependencyInfo& other):
       DependencyGraph::DependencyInfo(other) {}
 		const DependencyInfo& operator|=(const DependencyInfo& other);
     std::ostream& print(std::ostream& o) const;
@@ -152,26 +172,35 @@ public:
   // members
   //////////////////////////////////////////////////////////////////////////////
 protected:
-  // for debugging and printing
+  /** \brief Registry used for debugging and printing. */
   RegistryPtr reg;
   #ifdef COMPGRAPH_SOURCESDEBUG
-  // in non-debug mode this graph's lifetime can end
-  // after the constructor finished
+  /** \brief In non-debug mode this graph's lifetime can end
+    * after the constructor finished. */
   const DependencyGraph& dg;
   #endif
+  /** \brief Internal component graph. */
   Graph cg;
 
   //////////////////////////////////////////////////////////////////////////////
   // methods
   //////////////////////////////////////////////////////////////////////////////
 protected:
-	// only to be used by explicit clone method
+	/** \brief Copy-constructor.
+          *
+          * Only to be used by explicit clone method.
+          * @param other Component to copy. */
 	ComponentGraph(const ComponentGraph& other);
 public:
+	/** \brief Constructor to construct a component graph out of a DependencyGraph.
+          * @param dp DependencyGraph used as basis for the ComponentGraph.
+	  * @param reg See ComponentGraph::reg. */
 	ComponentGraph(const DependencyGraph& dg, RegistryPtr reg);
+	/** \brief Destructor. */
 	virtual ~ComponentGraph();
 
-	// for explicit cloning of the graph
+	/** \brief For explicit cloning of the graph.
+	  * @return Pointer to a copy of this ComponentGraph. */
 	ComponentGraph* clone() const;
 
 	//
@@ -188,15 +217,20 @@ public:
   // mighty helper for collapsing components
   //
 
-  // Compute the dependency infos and component info
-  // before putting components `comps' and `sharedcomps' into a new component.
-  //
-  // `sharedcomps' may only contain components with constraints that can be shared.
-  //
-  // This method does not change the graph, it only changes the output arguments,
-  // hence it is const (and should stay const).
-  // 
-  // This method throws an exception if this operation makes the DAG cyclic.
+  /** \brief Compute the dependency infos and component info
+    * before putting components `comps' and `sharedcomps' into a new component.
+    *
+    * `sharedcomps' may only contain components with constraints that can be shared.
+    *
+    * This method does not change the graph, it only changes the output arguments,
+    * hence it is const (and should stay const).
+    *
+    * This method throws an exception if this operation makes the DAG cyclic.
+    * @param comps Set of overall components.
+    * @param sharedcomps Set of components which are shared among multiple evaluation units.
+    * @param newIncomingDependencies Collapsed incoming dependencies of the collapsed component.
+    * @param newOutgoingDependencies Collapsed outgoing dependencies of the collapsed component.
+    * @param newComponentInfo New ComponentInfo of the collapsed component. */
   void computeCollapsedComponentInfos(
       const ComponentSet& comps, const ComponentSet& sharedcomps,
       DepMap& newIncomingDependencies, DepMap& newOutgoingDependencies,
@@ -210,54 +244,87 @@ public:
 	inline const Graph& getInternalGraph()
 		const { return cg; }
 
-  // output graph as graphviz source
+  /** \brief Output graph as graphviz source (dot file).
+    * @param o Stream to print the graph to.
+    * @param verbose True to include more information. */
   virtual void writeGraphViz(std::ostream& o, bool verbose) const;
 
-  // get range over all components
+  /** \brief Get range over all components.
+    * @return Pair of begin and end iterator. */
   inline std::pair<ComponentIterator, ComponentIterator> getComponents() const
     { return boost::vertices(cg); }
+  /** \brief Get range over all edges.
+    * @return Pair of begin and end iterator. */
   inline std::pair<DependencyIterator, DependencyIterator> getDependencies() const
     { return boost::edges(cg); }
 
-	// get node info given node
+	/** \brief Get node info given node.
+	  * @param c Some component of the graph.
+	  * @return ComponentInfo of \p c.
+	  */
 	inline const ComponentInfo& getComponentInfo(Component c) const
 		{ return cg[c]; }
 
-	// get dependency info given dependency
+	/** \brief Get dependency info given dependency.
+	  * @param dep Some dependency of the graph.
+	  * @return DependencyInfo of \p dep. */
 	inline const DependencyInfo& getDependencyInfo(Dependency dep) const
 		{ return cg[dep]; }
 
-	// get dependencies (to predecessors) = arcs from this component to others
+	/** \brief Get dependencies (to predecessors) = arcs from this component to others.
+	  * @param c Some component of the graph.
+	  * @return Pair of begin and end iterator. */
   inline std::pair<PredecessorIterator, PredecessorIterator>
   getDependencies(Component c) const
 		{ return boost::out_edges(c, cg); }
 
-	// get provides (dependencies to successors) = arcs from other component to this one
+	/** \brief Get provides (dependencies to successors) = arcs from other component to this one.
+	  * @param c Some component of the graph.
+	  * @return PAir of begin and end iterator. */
   inline std::pair<SuccessorIterator, SuccessorIterator>
   getProvides(Component c) const
 		{ return boost::in_edges(c, cg); }
 
-	// get source of dependency = component that depends
+	/** \brief Get source of dependency = component that depends.
+	  * @param d Some dependency of the graph.
+	  * @return Component where \p d origins. */
   inline Component sourceOf(Dependency d) const
 		{ return boost::source(d, cg); }
 
-	// get target of dependency = component upon which the source depends
+	/** \brief Get target of dependency = component upon which the source depends.
+	  * @param d Some dependency of the graph.
+	  * @return Component where \p d ends. */
   inline Component targetOf(Dependency d) const
 		{ return boost::target(d, cg); }
 
-	// get node/dependency properties
+	/** \brief Get node properties.
+	  * @param c Some component of the graph.
+	  * @return ComponentInfo of \p c. */
 	inline const ComponentInfo& propsOf(Component c) const
 		{ return cg[c]; }
+	/** \brief Get node properties.
+	  * @param c Some component of the graph.
+	  * @return ComponentInfo of \p c. */
 	inline ComponentInfo& propsOf(Component c)
 		{ return cg[c]; }
+	/** \brief Get dependency properties.
+	  * @param d Some dependency of the graph.
+	  * @return ComponentInfo of \p d. */
 	inline const DependencyInfo& propsOf(Dependency d) const
 		{ return cg[d]; }
+	/** \brief Get dependency properties.
+	  * @param d Some dependency of the graph.
+	  * @return ComponentInfo of \p d. */
 	inline DependencyInfo& propsOf(Dependency d)
 		{ return cg[d]; }
 
 	// counting -> mainly for allocating and testing
+  /** \brief Retrieves the number of components.
+    * @return Number of components. */
   inline unsigned countComponents() const
 		{ return boost::num_vertices(cg); }
+  /** \brief Retrieves the number of depdencies.
+    * @return Number of depdencies. */
   inline unsigned countDependencies() const
 		{ return boost::num_edges(cg); }
 
@@ -266,17 +333,37 @@ public:
 	//
 protected:
   // helpers for writeGraphViz: extend for more output
+  /** \brief Writes a single component in dot format.
+    * @param o Stream to write to.
+    * @param c Component to output.
+    * @param index Index to use for identifying the component.
+    * @param verbose True to include more detailed information. */
   virtual void writeGraphVizComponentLabel(std::ostream& o, Component c, unsigned index, bool verbose) const;
+  /** \brief Writes a single dependency in dot format.
+    * @param o Stream to write to.
+    * @param dep Dependency to output.
+    * @param verbose True to include more detailed information. */
   virtual void writeGraphVizDependencyLabel(std::ostream& o, Dependency dep, bool verbose) const;
 
 protected:
   // helpers for constructor
+  /** \brief Computes the meta information about the dependencies in the graph.
+    * @param dp DependencyGraph to compute the information for. */
   void calculateComponents(const DependencyGraph& dg);
 
+  /** \brief Checks if a given component uses value invention.
+    * @param ci ComponentInfo of the component to check.
+    * @return False if \p ci uses value invention and true otherwise. */
   bool calculateFixedDomain(ComponentInfo& ci);
+  /** \brief Checks if a given component uses recursive aggregates.
+    * @param ci ComponentInfo of the component to check.
+    * @return True if \p ci uses recursive aggregates and false otherwise. */
   bool computeRecursiveAggregatesInComponent(ComponentInfo& ci);
 
 public:
+  /** \brief Computes stratification info for a component and stores it in the graph.
+    * @param reg Registry used for resolving IDs.
+    * @param ci ComponentInfo of the component to check. */
   static void calculateStratificationInfo(RegistryPtr reg, ComponentInfo& ci);
 };
 
--- include/dlvhex2/EvalGraph.h
+++ include/dlvhex2/EvalGraph.h
@@ -52,10 +52,8 @@ struct none_t
   inline std::ostream& print(std::ostream& o) const { return o; }
 };
 
-//
-// the EvalGraph template manages a generic evaluation graph:
-// it takes care of a correct join order among in-edges of units
-//
+/** \brief The EvalGraph template manages a generic evaluation graph:
+  * it takes care of a correct join order among in-edges of units, */
 template<
   typename EvalUnitPropertyBaseT = none_t,
   typename EvalUnitDepPropertyBaseT = none_t>
@@ -68,10 +66,13 @@ public:
   typedef EvalUnitPropertyBaseT EvalUnitPropertyBase;
   typedef EvalUnitDepPropertyBaseT EvalUnitDepPropertyBase;
 
+  /** \brief Couples an EvalUnit with its properties. */
   struct EvalUnitPropertyBundle:
     public EvalUnitPropertyBase,
     public ostream_printable<EvalUnitPropertyBundle>
   {
+    /** \brief Constructor.
+     * @param base Properties. */
     EvalUnitPropertyBundle(
       const EvalUnitPropertyBase& base = EvalUnitPropertyBase()):
         EvalUnitPropertyBase(base) {}
@@ -79,17 +80,24 @@ public:
     std::ostream& print(std::ostream& o) const
       { return o << static_cast<const EvalUnitPropertyBase>(*this); }
   };
+  /** \brief Couples an EvalUnitDep with its properties. */
   struct EvalUnitDepPropertyBundle:
     public EvalUnitDepPropertyBaseT,
     public ostream_printable<EvalUnitDepPropertyBundle>
   {
     // storage
+    /** \brief Join order. */
     unsigned joinOrder;
 
     // init
+    /** \brief Constructor.
+      * @param joinOrder Join order. */
     EvalUnitDepPropertyBundle(
       unsigned joinOrder = 0):
         joinOrder(joinOrder) {}
+    /** \brief Constructor.
+      * @param base Properties.
+      * @param joinOrder Join order. */
     EvalUnitDepPropertyBundle(
       const EvalUnitDepPropertyBase& base,
       unsigned joinOrder = 0):
@@ -126,11 +134,17 @@ public:
   typedef typename Traits::out_edge_iterator PredecessorIterator;
   typedef typename Traits::in_edge_iterator SuccessorIterator;
 
+  /** \brief Observer. */
   class Observer
   {
   public:
+    /** \brief Destructor. */
     virtual ~Observer() {}
+    /** \brief Adds an evaluation unit.
+      * @param u Evaluation unit to add. */
     virtual void addUnit(EvalUnit u) = 0;
+    /** \brief Adds a dependency.
+      * @param d Dependency to add. */
     virtual void addDependency(EvalUnitDep d) = 0;
   };
   typedef boost::shared_ptr<Observer> ObserverPtr;
@@ -146,14 +160,20 @@ private:
   // methods
   //////////////////////////////////////////////////////////////////////////////
 public:
+	/** \brief Constructor. */
 	EvalGraph()
     {}
-  // this is not implemented on purpose (=linker error) because it is forbidden to use
-  // (it cannot be private as this would cause concept checks to fail (TODO: check why clang produces such bad error messages in this case)
+  /** \brief Copy-constructor.
+    *
+    * This is not implemented on purpose (=linker error) because it is forbidden to use
+    * (it cannot be private as this would cause concept checks to fail (TODO: check why clang produces such bad error messages in this case).
+    * @param other Second EvalGraph. */
 	EvalGraph(const EvalGraph& other);
   inline const EvalGraphInt& getInt() const
     { return eg; }
 
+  /** \brief Adds an evaluation unit with properties.
+    * @param prop Evaluation unit and properties to add. */
   inline EvalUnit addUnit(const EvalUnitPropertyBundle& prop)
   {
     EvalUnit u = boost::add_vertex(prop, eg);
@@ -162,6 +182,10 @@ public:
     return u;
   }
 
+  /** \brief Adds a dependency.
+    * @param u1 From unit.
+    * @param u2 To unit.
+    * @param prop Properties of the dependency. */
   inline EvalUnitDep addDependency(EvalUnit u1, EvalUnit u2,
     const EvalUnitDepPropertyBundle& prop)
   {
@@ -193,16 +217,22 @@ public:
     return dep;
   }
 
+  /** \brief Adds an observer.
+    * @param o Observer to add. */
   void addObserver(ObserverPtr o)
   {
     observers.insert(o);
   }
 
+  /** \brief Removes an observer.
+    * @param o Observer to remove. */
   void eraseObserver(ObserverPtr o)
   {
     observers.erase(o);
   }
 
+  /** \brief Retrieves all evaluation units.
+    * @return Pair of begin and end iterator. */
   inline std::pair<EvalUnitIterator, EvalUnitIterator>
   getEvalUnits() const
   {
@@ -217,76 +247,113 @@ public:
     return boost::out_edges(u, eg);
   }
 
-  // successors are eval units we provide input to,
-  // edges are dependencies, so successors are at incoming edges
+  /** \brief Successors are eval units we provide input to,
+    * edges are dependencies, so successors are at incoming edges.
+    * @param u Evaluation unit.
+    * @return Successors of \p u represented by a pair of begin and end iterator. */
   inline std::pair<SuccessorIterator, SuccessorIterator>
   getSuccessors(EvalUnit u) const
   {
     return boost::in_edges(u, eg);
   }
 
+  /** \brief Retrieves the properties of an evaluation unit dependency.
+    * @param d EvalUnitDep.
+    * @return Properties of \p d. */
   inline const EvalUnitDepPropertyBundle& propsOf(EvalUnitDep d) const
   {
     return eg[d];
   }
 
+  /** \brief Retrieves the properties of an evaluation unit dependency.
+    * @param d EvalUnitDep.
+    * @return Properties of \p d. */
   inline EvalUnitDepPropertyBundle& propsOf(EvalUnitDep d)
   {
     return eg[d];
   }
 
+  /** \brief Retrieves the properties of an evaluation unit.
+    * @param u EvalUnit.
+    * @return Properties of \p u. */
   inline const EvalUnitPropertyBundle& propsOf(EvalUnit u) const
   {
     return eg[u];
   }
 
+  /** \brief Retrieves the properties of an evaluation unit.
+    * @param u EvalUnit.
+    * @return Properties of \p u. */
   inline EvalUnitPropertyBundle& propsOf(EvalUnit u)
   {
     return eg[u];
   }
 
+  /** \brief Retrieves the source unit of a depedency.
+    * @param d EvalUnitDep.
+    * @return Source unit of \p d. */
   inline EvalUnit sourceOf(EvalUnitDep d) const
   {
     return boost::source(d, eg);
   }
+
+  /** \brief Retrieves the target unit of a depedency.
+    * @param d EvalUnitDep.
+    * @return Target unit of \p d. */
   inline EvalUnit targetOf(EvalUnitDep d) const
   {
     return boost::target(d, eg);
   }
 
+  /** \brief Returns the number of evaluation units.
+    * @return Number of evaluation units. */
   inline unsigned countEvalUnits() const
   {
     return boost::num_vertices(eg);
   }
+
+  /** \brief Returns the number of dependencies.
+    * @return Number of dependencies. */
   inline unsigned countEvalUnitDeps() const
   {
     return boost::num_edges(eg);
   }
 
-  // output graph as graphviz source
+  /** \brief Output graph as graphviz source (dot file).
+    * @param o Stream to print the graph to.
+    * @param verbose True to include more information. */
   void writeGraphViz(std::ostream& o, bool verbose) const;
 
 	struct Impl;
 }; // class EvalGraph<...>
 
-// projection properties for eval units
-// such properties are required by the model graph
+/** \brief Projection properties for eval units
+  * such properties are required by the model graph. */
 struct EvalUnitProjectionProperties
 {
   // storage
+  /** \brief Do input projection. */
   bool iproject;
+  /** \brief Do output projection. */
   bool oproject;
 
   // init
+  /** \brief Constructor.
+    * @param iproject See EvalUnitProjectionProperties::iproject.
+    * @param oproject See EvalUnitProjectionProperties::oproject. */
   EvalUnitProjectionProperties(
     bool iproject = false,
     bool oproject = false):
       iproject(iproject), oproject(oproject) {}
 };
 
+/** \brief Helper for printing a single EvalUnit in dot format. */
 template<typename EvalUnitPropertyBaseT, typename EvalUnitDepPropertyBaseT>
 struct EvalGraph<EvalUnitPropertyBaseT, EvalUnitDepPropertyBaseT>::Impl
 {
+  /** \brief Returns a string representation of a single EvalUnit.
+    * @param u EvalUnit to represent.
+    * @return String representation of \p u. */
   static std::string graphviz_node_id(EvalUnit u)
   {
     std::ostringstream os;
@@ -295,6 +362,9 @@ struct EvalGraph<EvalUnitPropertyBaseT, EvalUnitDepPropertyBaseT>::Impl
   }
 };
 
+/** \brief Helper for printing the graph in dot format.
+  * @param o Stream to write to.
+  * @param verbose True to include more detailed information. */
 template<typename EvalUnitPropertyBaseT, typename EvalUnitDepPropertyBaseT>
 void EvalGraph<EvalUnitPropertyBaseT, EvalUnitDepPropertyBaseT>::
 	writeGraphViz(std::ostream& o, bool verbose) const
