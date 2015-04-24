d358599efd411e837f2d60de7308a1d8e75e320e
Christoph Redl <redl@kr.tuwien.ac.at>
2015-04-24 17:43:39 +0200

    small fixes

--- src/ProgramCtx.cpp
+++ src/ProgramCtx.cpp
@@ -147,7 +147,7 @@ maxint(0), currentOptimumRelevantLevels(0), terminationRequest(false)
                                  // if 1 then we only show optimal results, otherwise before getting optimal results we might get nonoptimal ones
     config.setOption("OptimizationFilterNonOptimal", 1);
 
-    #warning "TODO cleanup the setASPSoftware vs nGenuineSolver thing"
+    WARNING("TODO cleanup the setASPSoftware vs nGenuineSolver thing")
     // but if we have genuinegc, take genuinegc as default
     #if defined(HAVE_LIBGRINGO) && defined(HAVE_LIBCLASP)
     config.setOption("GenuineSolver", 4);
