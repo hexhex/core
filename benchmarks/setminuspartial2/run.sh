fad0f46a513326a1a43281b9ed22ae0f64dba569
Christoph Redl <redl@kr.tuwien.ac.at>
2015-07-04 21:07:15 +0200

    add benchmarks for partial evaluation

---
+++ benchmarks/setminuspartial2/run.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+runheader=$(which dlvhex_run_header.sh)
+if [[ $runheader == "" ]] || [ $(cat $runheader | grep "dlvhex_run_header.sh Version 1." | wc -l) == 0 ]; then
+	echo "Could not find dlvhex_run_header.sh (version 1.x); make sure that the benchmarks/script directory is in your PATH"
+	exit 1
+fi
+source $runheader
+
+# run instances
+if [[ $all -eq 1 ]]; then
+	# run all instances using the benchmark script run insts
+	$bmscripts/runinsts.sh "{1..40}" "$mydir/run.sh" "$mydir" "$to" "" "" "$req"
+else
+	# run single instance
+	confstr=";--eaevalheuristics=always;-N=1;--eaevalheuristics=always -N=1"
+
+	# write instance file
+	inststr=`printf "%03d" ${instance}`
+	instfile=$(mktemp "inst_${inststr}_XXXXXXXXXX.hex")
+	if [[ $? -gt 0 ]]; then
+		echo "Error while creating temp file" >&2
+		exit 1
+	fi
+	prog="
+		sel(X) v nsel(X) :- domain(X).
+
+		result(X) :- domain(X), &testSetMinusPartial[sel, empty](X)."
+	for (( j = 1; j <= $instance; j++ ))
+	do
+		prog="domain($j). $prog"
+	done
+	echo $prog > $instfile
+
+	$bmscripts/runconfigs.sh "dlvhex2 --python-plugin=../../testsuite/plugin.py CONF INST" "$confstr" "$instfile" "$to"
+	rm $instfile
+fi
+
