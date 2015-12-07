7b37f366055f48d12f8e66c1e6ff41954b254db2
Kaminski <tobias.d.kaminski@gmail.com>
2015-12-07 12:19:24 +0100

    change strategic companies benchmark

--- benchmarks/strategiccompanies/generate_instance.sh
+++ benchmarks/strategiccompanies/generate_instance.sh
@@ -16,14 +16,17 @@ do
 	j2=$(( ($RANDOM%$1)+1 ))
 	j3=$(( ($RANDOM%$1)+1 ))
 	j4=$(( ($RANDOM%$1)+1 ))
-	echo "controlled_by(c$j1,c$j2,c$j3,c$j4)."
+	j5=$(( ($RANDOM%$1)+1 ))
+	echo "controlled_by(c$j1,c$j2,c$j3,c$j4,c$j5)."
 done
 
 for (( i=1; i <= $3; i++ ))
 do
 	j1=$(( ($RANDOM%$1)+1 ))
 	j2=$(( ($RANDOM%$1)+1 ))
-	echo "produced_by(p$i,c$j1,c$j2)."
+	j3=$(( ($RANDOM%$1)+1 ))
+	j4=$(( ($RANDOM%$1)+1 ))
+	echo "produced_by(p$i,c$j1,c$j2,c$j3,c$j4)."
 done
 
 for (( i=1; i <= $4; i++ ))
