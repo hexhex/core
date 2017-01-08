6de37321b73f93f1a2249cfe2a463e9e39d8a5bb
Christoph Redl <redl@kr.tuwien.ac.at>
2017-01-08 14:56:42 +0100

    update benchmark

--- benchmarks/genericmapping/run.sh
+++ benchmarks/genericmapping/run.sh
@@ -10,11 +10,11 @@ source $runheader
 # run instances
 if [[ $all -eq 1 ]]; then
 	# run all instances using the benchmark script run insts
-	$bmscripts/runinsts.sh "instances/inst_*.hex" "$mydir/run.sh" "$mydir" "$to" "$mydir/myagg.sh" "" "$req"
+	$bmscripts/runinsts.sh "instances/inst_*.hex" "$mydir/run.sh" "$mydir" "$to" "" "" "$req" # $mydir/myagg.sh
 else
 	# run single instance
 	confstr=";--heuristics=monolithic;--transunitlearning"
 
-	$bmscripts/runconfigs.sh "dlvhex2 --plugindir=../../testsuite --silent mapping.hex --ngminimization=always --verbose=8 CONF INST" "$confstr" "$instance" "$to" "$bmscripts/anscgstimeoutputbuilder.sh"
+	$bmscripts/runconfigs.sh "dlvhex2 --plugindir=../../testsuite --silent mapping.hex --ngminimization=always --verbose=8 CONF INST" "$confstr" "$instance" "$to" "$bmscripts/gstimeoutputbuilder.sh"
 fi
 
