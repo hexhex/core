0705e228cfb41a04e5a682c8709bb5141a0734a9
Christoph Redl <redl@kr.tuwien.ac.at>
2017-01-31 23:27:47 +0100

    adopt parameters

--- benchmarks/diagnosis/generateset.sh
+++ benchmarks/diagnosis/generateset.sh
@@ -13,12 +13,12 @@ do
 	for (( inst=0; inst < $3; inst++ ))
 	do
 		domsize=$size
-		potobsprop=50
+		potobsprop=80
 		tagdomsize=$(expr $size / 5 + 1)
 		maxdepsets=3 #$(expr $size + $size / 2)
 		tagprop=100
 		depprop=10
-		maxcons=$(expr $size \* 10)
+		maxcons=$size #$(expr $size \* 10)
 		consprop=20
 		conselemprop=30
 		./generate.sh $domsize $potobsprop $tagdomsize $maxdepsets $tagprop $depprop $maxcons $consprop $conselemprop > "instances/inst_size_${size}_inst_${inst}.hex"
