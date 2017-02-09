d055a273a3fe53860b8bdb0080d2334583ffe81b
Tobias Kaminski <tobias.d.kaminski@gmail.com>
2017-02-09 17:40:55 +0100

    update benchmark postoffice2

--- benchmarks/postoffice2/generateset.sh
+++ benchmarks/postoffice2/generateset.sh
@@ -1,22 +1,24 @@
 # $1/$2: min/max instance size
 # $3: number of instances
+# $4: step size
 
-if [[ $# -lt 3 ]]; then
+
+if [[ $# -lt 4 ]]; then
 	echo "Error: Script expects 3 parameters"
 	exit 1;
 fi
 
 # create a directory for storing benchmark instances
 mkdir -p instances
-for (( size=$1; size <= $2; size++ ))
+for (( size=$1; size <= $2; size = size + $4 ))
 do
 	for (( inst=0; inst < $3; inst++ ))
 	do
 		locations=$size
-                regions=$(( ($size/3) + 1 ))
+                regions=$(( ($size/4) + 1 ))
 		maxdist=10
-		maxsize=$(( ($size/$regions)*2 ))
-		maxdistallowed=4
+		maxsize=$(( ($size/($regions)) ))
+		maxdistallowed=8
 		./generate.sh $locations $regions $maxdist $maxsize $maxdistallowed > "instances/inst_size_${size}_inst_${inst}.hex"
 	done
 done
