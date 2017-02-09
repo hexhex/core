bfeab42ea391633577a6b772476899fcccb6d506
Tobias Kaminski <tobias.d.kaminski@gmail.com>
2017-02-09 12:43:13 +0100

    add variant of postoffice benchmark taking distances into account

---
+++ benchmarks/postoffice2/generateset.sh
@@ -0,0 +1,22 @@
+# $1/$2: min/max instance size
+# $3: number of instances
+
+if [[ $# -lt 3 ]]; then
+	echo "Error: Script expects 3 parameters"
+	exit 1;
+fi
+
+# create a directory for storing benchmark instances
+mkdir -p instances
+for (( size=$1; size <= $2; size++ ))
+do
+	for (( inst=0; inst < $3; inst++ ))
+	do
+		locations=$size
+                regions=$(expr $size / 5 + 1)
+		maxdist=10
+		maxsize=$(expr $size / $regions \* 2)
+		maxdistallowed=5
+		./generate.sh $locations $regions $maxdist $maxsize $maxdistallowed > "instances/inst_size_${size}_inst_${inst}.hex"
+	done
+done
