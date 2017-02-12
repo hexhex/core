256e547a89ea4de4373071ae4e813cee11f71f56
Tobias Kaminski <tobias.d.kaminski@gmail.com>
2017-02-12 22:31:23 +0100

    small fix

--- benchmarks/postoffice3/generateset.sh
+++ benchmarks/postoffice3/generateset.sh
@@ -1,13 +1,14 @@
 # $1: min instance size (number of locations)
 # $2: max instance size (number of locations)
 # $3: step size (number of locations)
-# $4: max allowed distance to region (max dist)
-# $5: max distance to region (range of distances of locations to regions: 0..$7)
-# $6: number of instances
+# $4: number of regions
+# $5: max allowed distance to region (max dist)
+# $6: max distance to region (range of distances of locations to regions: 0..$7)
+# $7: number of instances
 
 
 if [[ $# -lt 6 ]]; then
-	echo "Error: Script expects 6 parameters"
+	echo "Error: Script expects 7 parameters"
 	exit 1;
 fi
 
@@ -17,9 +18,9 @@ for (( size=$1; size <= $2; size = size + $3 ))
 do
 	for (( inst=0; inst < $6; inst++ ))
 	do
-		regions=2
+		regions=$4
 		maxsize=$(( ($size/2) +1 ))
-		./generate.sh $regions $size $maxsize $4 $5 > "instances/inst_size_${size}_${4}_${5}_inst_${inst}.hex"
+		./generate.sh $regions $size $maxsize $5 $6 > "instances/inst_size_${size}_regions_${4}_${5}_${6}_inst_${inst}.hex"
 	done
 done
 
