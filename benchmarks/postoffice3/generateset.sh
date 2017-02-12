44610a7c8eb694ea50b70b2e8923ceedcbe4ef7d
Tobias Kaminski <tobias.d.kaminski@gmail.com>
2017-02-12 19:59:21 +0100

    small fix

--- benchmarks/postoffice3/generateset.sh
+++ benchmarks/postoffice3/generateset.sh
@@ -2,7 +2,7 @@
 # $2: max instance size (number of regions)
 # $3: step size (number of regions)
 # $4: location number factor (number of locations = regions*$4)
-# $5: max locations assigned to region (max size)
+# $5: max locations assigned to region factor (max size = regions*$5)
 # $6: max allowed distance to region (max dist)
 # $7: max distance to region (range of distances of locations to regions: 0..$7)
 # $8: number of instances
@@ -20,7 +20,8 @@ do
 	for (( inst=0; inst < $8; inst++ ))
 	do
 		locations=$(( $size*$4 ))
-		./generate.sh $size $locations $5 $6 $7 > "instances/inst_size_${size}_${5}_${6}_${7}_inst_${inst}.hex"
+		maxsize=$(( $size*$5 ))
+		./generate.sh $size $locations $maxsize $6 $7 > "instances/inst_size_${size}_${5}_${6}_${7}_inst_${inst}.hex"
 	done
 done
 
