566104c31c16fca46d3e1fbd514e0fa95d44c582
Tobias Kaminski <tobias.d.kaminski@gmail.com>
2017-09-19 12:01:39 +0200

    fix

--- src/ExternalLearningHelper.cpp
+++ src/ExternalLearningHelper.cpp
@@ -390,7 +390,7 @@ void ExternalLearningHelper::learnFromInputOutputBehavior(const PluginAtom::Quer
                         }
                     }
 
-                    if (query.inputi->getFact(ansID.address) || !query.ctx->config.getOption("MinimizeNogoodsOnConflict")) {
+                    if (!query.ctx->config.getOption("MinimizeNogoodsOnConflict") || (query.inputi && query.inputi->getFact(ansID.address))) {
                         DBGLOG(DBG, "Conflicting nogood");
 
                         testNg.erase(ansID);
@@ -471,7 +471,7 @@ void ExternalLearningHelper::learnFromInputOutputBehavior(const PluginAtom::Quer
                 std::map<std::size_t, PluginAtom::Answer> externalEvaluationsCache;
 
                 for (int i = 0; i < newNogoods.size(); ++i) {
-                    if ((query.inputi->getFact(newNogoods[i].second.address) || !query.ctx->config.getOption("MinimizeNogoodsOnConflict"))
+                    if ((!query.ctx->config.getOption("MinimizeNogoodsOnConflict") || (query.inputi && query.inputi->getFact(newNogoods[i].second.address)))
                             && (newNogoods[i].first.size() <= query.ctx->config.getOption("MinimizationSize"))) {
                         Nogood testNg = newNogoods[i].first;
 
@@ -716,7 +716,7 @@ void ExternalLearningHelper::learnFromNegativeAtoms(const PluginAtom::Query& que
                         }
                     }
 
-                    if (query.inputi->getFact(ansID.address) || !query.ctx->config.getOption("MinimizeNogoodsOnConflict")) {
+                    if (!query.ctx->config.getOption("MinimizeNogoodsOnConflict") || (query.inputi && query.inputi->getFact(ansID.address))) {
                         DBGLOG(DBG, "Conflicting nogood");
 
                         testNg.erase(ansID);
@@ -797,7 +797,7 @@ void ExternalLearningHelper::learnFromNegativeAtoms(const PluginAtom::Query& que
                 std::map<std::size_t, PluginAtom::Answer> externalEvaluationsCache;
 
                 for (int i = 0; i < newNogoods.size(); ++i) {
-                    if ((query.inputi->getFact(newNogoods[i].second.address) || !query.ctx->config.getOption("MinimizeNogoodsOnConflict"))
+                    if (!query.ctx->config.getOption("MinimizeNogoodsOnConflict") || ((query.inputi && query.inputi->getFact(newNogoods[i].second.address)))
                             && (newNogoods[i].first.size() <= query.ctx->config.getOption("MinimizationSize"))) {
                         Nogood testNg = newNogoods[i].first;
 
