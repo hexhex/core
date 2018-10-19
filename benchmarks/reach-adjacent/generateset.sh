182f532cd318bb2d561e7f3e64608d204f9fb815
Tobias Kaminski <tobias.d.kaminski@gmail.com>
2018-10-19 13:04:13 +0200

    fix

--- benchmarks/reach-adjacent/generateset.sh
+++ benchmarks/reach-adjacent/generateset.sh
@@ -4,8 +4,8 @@ do
 	ep=`printf "%03d" ${2}`
 	ac=`printf "%03d" ${1}`
 	in=`printf "%03d" ${inst}`
-	./generate.sh $1 $2 > "instances/graphinst_nodecount_${ac}_edgeprob_${ep}_inst_${in}.graph"
-	./generate_inst.sh $1 $ep $ac $in > "instances/graphinst_edgeprob_${ep}_nodecount_${ac}_inst_${in}.hex"
+	./generate.sh $1 $2 > "instances/graphinst_edgeprob_${ep}_nodecount_${ac}_inst_${in}.graph"
+	./generate_inst.sh $1 $ep $ac $in > "instances/graphinst_nodecount_${ac}_edgeprob_${ep}_inst_${in}.hex"
 
 done
 
