78607f562da9918097d06d53bff7c06a58de9c3a
Tobias Kaminski <tobias.d.kaminski@gmail.com>
2018-10-22 12:46:02 +0200

    fix

--- benchmarks/reach-adjacent/generate.sh
+++ benchmarks/reach-adjacent/generate.sh
@@ -1,9 +1,8 @@
 # $1: node count
 
-frac=$(((1 * 100) / $1))
+frac=$(((1 * 100) / ($1/2)))
 prop=$(((32768 * $frac)/100))
 
-echo $prop
 for (( i=1; i <= $1; i++ ))
 do
 	for (( j = 1; j <= $1; j++ ))
